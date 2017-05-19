7a6487eae9e68735d1fbf489bcd95207d0190507
Julian Thijssen <julianthijssen@gmail.com>
2017-05-19 15:24:00 +0200

    Document data consumer

--- Cytosplore/src/DataConsumer.h
+++ Cytosplore/src/DataConsumer.h
@@ -11,8 +11,21 @@ namespace plugin
 class DataConsumer
 {
 public:
+    /**
+    * Callback function which gets triggered when a new dataset is created.
+    * @param data - The dataset which was created.
+    */
     virtual void dataAdded(const DataTypePlugin& data) = 0;
+
+    /**
+    * Callback function which gets triggered when a dataset is changed.
+    * @param data - The dataset which was changed.
+    */
     virtual void dataChanged(const DataTypePlugin& data) = 0;
+
+    /**
+    * Callback function which gets triggered when a dataset is removed.
+    */
     virtual void dataRemoved() = 0;
 };
 
