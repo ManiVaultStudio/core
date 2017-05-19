2ee2e2cbc68529c2f38ae3d1a5c8181cd083ba3a
Julian Thijssen <julianthijssen@gmail.com>
2017-05-19 15:23:54 +0200

    Document plugin base class

--- Cytosplore/src/Plugin.h
+++ Cytosplore/src/Plugin.h
@@ -16,12 +16,31 @@ public:
     Plugin(Type type, QString name) : _type(type), _name(name) { }
     virtual ~Plugin() {};
 
+    /**
+     * Can be implemented to initialize the plugin to a certain state.
+     * This function gets called when the plugin is first instantiated.
+     */
     virtual void init() = 0;
 	
+    /** Returns the type of plugin, see all types in PluginType */
     Type getType() const { return _type; }
+
+    /**
+     * Returns the kind of plugin. The kind is specific to the
+     * particular implementation of a plugin type.
+     */
     QString getName() const { return _name; }
+
+    /**
+     * Returns the version of the plugin. If no version string is implemented 
+     * by this particular plugin it will return the "No version" string.
+     */
     virtual QString getVersion() const { return "No Version"; }
 
+    /**
+     * Stores a reference to the core in the plugin via the CoreInterface
+     * class, which provides restricted access to the core.
+     */
     void setCore(CoreInterface* core) {
         this->_core = core;
     }
