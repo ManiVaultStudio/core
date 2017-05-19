9d3ca0fd23ca13a5fc57efbd60e91c71d7cad875
Julian Thijssen <julianthijssen@gmail.com>
2017-05-19 11:31:57 +0200

    Remove data generation from Points2DPlugin

--- Points2DPlugin/src/Points2DPlugin.cpp
+++ Points2DPlugin/src/Points2DPlugin.cpp
@@ -16,21 +16,7 @@ Points2DPlugin::~Points2DPlugin(void)
 
 void Points2DPlugin::init()
 {
-    srand(0);
-    for (int i = 0; i < 500000; i++)
-    {
-        float x = (float)(rand() % 30000 - 15000) / 15000;
-        float y = (float)(rand() % 30000 - 15000) / 15000;
 
-        float len = sqrt(x*x + y*y);
-        float roulette = (float)(rand() % 30000 - 15000) / 15000;
-        if (roulette < len * 1.4) {
-            continue;
-        }
-        data.push_back(x);
-        data.push_back(y);
-    }
-    qDebug() << "Generated: " << data.size() << " points.";
 }
 
 // =============================================================================
