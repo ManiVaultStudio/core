591590bc6beab95dfe8aebbd16570a86772fa180
Julian Thijssen <julianthijssen@gmail.com>
2017-06-20 13:13:58 +0200

    Make scatterplot plugin handle both point and selection data

--- ScatterplotPlugin/src/ScatterplotPlugin.cpp
+++ ScatterplotPlugin/src/ScatterplotPlugin.cpp
@@ -1,6 +1,7 @@
 #include "ScatterplotPlugin.h"
 
 #include "PointsPlugin.h"
+#include "SelectionPlugin.h"
 
 #include <QtCore>
 #include <QtDebug>
@@ -31,7 +32,7 @@ void ScatterplotPlugin::init()
 void ScatterplotPlugin::dataAdded(const QString name)
 {
     DataTypePlugin* data = _core->requestData(name);
-    if (data->getKind() == "Points") {
+    if (data->getKind() == "Points" || data->getKind() == "Selection") {
         dataOptions.addItem(name);
     }
 }
@@ -64,22 +65,42 @@ void ScatterplotPlugin::dataSetPicked(const QString& name)
 void ScatterplotPlugin::updateData(const QString name)
 {
     DataTypePlugin* data = _core->requestData(name);
-    if (data->getKind() != "Points") {
-        qWarning((QString("Scatterplot plugin doesn't support data of kind: ") + data->getKind()).toStdString().c_str());
-        return;
-    }
-
-    const PointsPlugin* points = dynamic_cast<const PointsPlugin*>(data);
-    qDebug() << "Data Changed for scatterplot: " << points->data.size();
-
+    
     std::vector<float>* positions = new std::vector<float>();
     std::vector<float> colors;
-    for (int i = 0; i < points->data.size() / 5; i++) {
-        positions->push_back(points->data[i * 5 + 0]);
-        positions->push_back(points->data[i * 5 + 1]);
-        colors.push_back(points->data[i * 5 + 2]);
-        colors.push_back(points->data[i * 5 + 3]);
-        colors.push_back(points->data[i * 5 + 4]);
+
+    if (data->getKind() == "Points")
+    {
+        const PointsPlugin* points = dynamic_cast<const PointsPlugin*>(data);
+
+        for (int i = 0; i < points->data.size() / 5; i++)
+        {
+            positions->push_back(points->data[i * 5 + 0]);
+            positions->push_back(points->data[i * 5 + 1]);
+            colors.push_back(points->data[i * 5 + 2]);
+            colors.push_back(points->data[i * 5 + 3]);
+            colors.push_back(points->data[i * 5 + 4]);
+        }
+    }
+    else if (data->getKind() == "Selection")
+    {
+        const SelectionPlugin* selection = dynamic_cast<const SelectionPlugin*>(data);
+
+        const PointsPlugin* points = dynamic_cast<const PointsPlugin*>(_core->requestData(selection->parentName));
+
+        for (unsigned int index : selection->indices)
+        {
+            positions->push_back(points->data[index * 5 + 0]);
+            positions->push_back(points->data[index * 5 + 1]);
+            colors.push_back(points->data[index * 5 + 2]);
+            colors.push_back(points->data[index * 5 + 3]);
+            colors.push_back(points->data[index * 5 + 4]);
+        }
+    }
+    else
+    {
+        qWarning((QString("Scatterplot plugin doesn't support data of kind: ") + data->getKind()).toStdString().c_str());
+        return;
     }
 
     widget->setData(positions);
@@ -88,20 +109,33 @@ void ScatterplotPlugin::updateData(const QString name)
 
 void ScatterplotPlugin::onSelection(const std::vector<unsigned int> selection) const
 {
-    QString name = _core->addData("Points");
+    //QString name = _core->addData("Points");
 
-    DataTypePlugin* currentData = _core->requestData(dataOptions.currentText());
-    const PointsPlugin* currentPoints = dynamic_cast<const PointsPlugin*>(currentData);
+    //DataTypePlugin* currentData = _core->requestData(dataOptions.currentText());
+    //const PointsPlugin* currentPoints = dynamic_cast<const PointsPlugin*>(currentData);
+
+    //DataTypePlugin* selectionData = _core->requestData(name);
+    //PointsPlugin* selectionPoints = dynamic_cast<PointsPlugin*>(selectionData);
+
+    //for (unsigned int index : selection) {
+    //    selectionPoints->data.push_back(currentPoints->data[index * 5 + 0]);
+    //    selectionPoints->data.push_back(currentPoints->data[index * 5 + 1]);
+    //    selectionPoints->data.push_back(currentPoints->data[index * 5 + 2]);
+    //    selectionPoints->data.push_back(currentPoints->data[index * 5 + 3]);
+    //    selectionPoints->data.push_back(currentPoints->data[index * 5 + 4]);
+    //}
+
+    //_core->notifyDataChanged(name);
+
+
+    QString name = _core->addData("Selection");
 
     DataTypePlugin* selectionData = _core->requestData(name);
-    PointsPlugin* selectionPoints = dynamic_cast<PointsPlugin*>(selectionData);
+    SelectionPlugin* selectionPlugin = dynamic_cast<SelectionPlugin*>(selectionData);
 
+    selectionPlugin->parentName = dataOptions.currentText();
     for (unsigned int index : selection) {
-        selectionPoints->data.push_back(currentPoints->data[index * 5 + 0]);
-        selectionPoints->data.push_back(currentPoints->data[index * 5 + 1]);
-        selectionPoints->data.push_back(currentPoints->data[index * 5 + 2]);
-        selectionPoints->data.push_back(currentPoints->data[index * 5 + 3]);
-        selectionPoints->data.push_back(currentPoints->data[index * 5 + 4]);
+        selectionPlugin->indices.push_back(index);
     }
 
     _core->notifyDataChanged(name);
