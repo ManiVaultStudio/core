2e51bad52a83e6df66be4cc8d123eb1baaab0b8d
Julian Thijssen <Nimthora@gmail.com>
2017-08-18 02:26:08 +0200

    Add file loading utility function to FileUtil

---
+++ HDPS/src/util/FileUtil.cpp
@@ -0,0 +1,27 @@
+#ifndef HDPS_UTIL_FILE_UTIL_H
+#define HDPS_UTIL_FILE_UTIL_H
+
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+
+namespace hdps
+{
+namespace util
+{
+    QString loadFileContents(QString path) {
+        QFile file(path);
+
+        QString contents = "";
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QTextStream stream(&file);
+            contents = stream.readAll();
+        }
+
+        return contents;
+    }
+}
+}
+
+#endif // HDPS_UTIL_FILE_UTIL_H
---
+++ HDPS/src/util/FileUtil.h
@@ -0,0 +1,14 @@
+#ifndef HDPS_UTIL_FILE_UTIL_H
+#define HDPS_UTIL_FILE_UTIL_H
+
+#include <QString>
+
+namespace hdps
+{
+namespace util
+{
+    QString loadFileContents(QString path);
+}
+}
+
+#endif // HDPS_UTIL_FILE_UTIL_H
