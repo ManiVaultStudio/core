67c7e6cdbee6d46bf8bdda088a5fa3a04631635c
Julian Thijssen <julianthijssen@gmail.com>
2017-10-31 12:51:25 +0100

    Add operator overloads to Vector2f

--- HDPS/src/graphics/Vector2f.h
+++ HDPS/src/graphics/Vector2f.h
@@ -40,6 +40,82 @@ namespace hdps
             return sqrt(sqrMagnitude());
         }
 
+        /* Operator overloads */
+        bool operator==(const Vector2f& v) const
+        {
+            return x == v.x && y == v.y;
+        }
+
+        bool operator!=(const Vector2f& v) const
+        {
+            return x != v.x || y != v.y;
+        }
+
+        Vector2f& operator+=(const Vector2f& v)
+        {
+            x += v.x;
+            y += v.y;
+            return *this;
+        }
+
+        Vector2f& operator-=(const Vector2f& v)
+        {
+            x -= v.x;
+            y -= v.y;
+            return *this;
+        }
+
+        Vector2f& operator*=(const Vector2f& v)
+        {
+            x *= v.x;
+            y *= v.y;
+            return *this;
+        }
+
+        Vector2f& operator*=(float scale)
+        {
+            x *= scale;
+            y *= scale;
+            return *this;
+        }
+
+        Vector2f& operator/=(const Vector2f& v)
+        {
+            x /= v.x;
+            y /= v.y;
+            return *this;
+        }
+
+        Vector2f operator+(const Vector2f& v) const
+        {
+            return Vector2f(x + v.x, y + v.y);
+        }
+
+        Vector2f operator-(const Vector2f& v) const
+        {
+            return Vector2f(x - v.x, y - v.y);
+        }
+
+        Vector2f operator-() const
+        {
+            return Vector2f(-x, -y);
+        }
+
+        Vector2f operator*(const Vector2f& v) const
+        {
+            return Vector2f(x * v.x, y * v.y);
+        }
+
+        Vector2f operator*(float scale) const
+        {
+            return Vector2f(x * scale, y * scale);
+        }
+
+        Vector2f operator/(float divisor) const
+        {
+            return Vector2f(x / divisor, y / divisor);
+        }
+
         float x, y;
     };
 
