a40e0cd6252496489f9246d263bf09415c9701d6
Julian Thijssen <julianthijssen@gmail.com>
2017-10-31 12:39:44 +0100

    Add Vector2f class

---
+++ HDPS/src/graphics/Vector2f.h
@@ -0,0 +1,47 @@
+#ifndef VECTOR2F_H
+#define VECTOR2F_H
+
+#include <cmath>
+
+namespace hdps
+{
+    class Vector2f
+    {
+    public:
+        Vector2f() : x(0), y(0) { }
+        ~Vector2f();
+
+        inline void set(float x, float y)
+        {
+            this->x = x;
+            this->y = y;
+        }
+
+        inline void add(Vector2f v)
+        {
+            x += v.x;
+            y += v.y;
+        }
+
+        inline void sub(Vector2f v)
+        {
+            x -= v.x;
+            y -= v.y;
+        }
+
+        inline float sqrMagnitude()
+        {
+            return x * x + y * y;
+        }
+
+        inline float length()
+        {
+            return sqrt(sqrMagnitude());
+        }
+
+        float x, y;
+    };
+
+} // namespace hdps
+
+#endif // VECTOR2F_H
