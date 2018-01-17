99b400d5a7b15067816695c1e11c877c489e403f
Julian Thijssen <julianthijssen@gmail.com>
2018-01-17 14:14:26 +0100

    Make Texture class not automatically create and destroy its OpenGL resources

--- HDPS/src/graphics/Texture.h
+++ HDPS/src/graphics/Texture.h
@@ -2,25 +2,22 @@
 #define TEXTURE_H
 
 #include <QOpenGLFunctions_3_3_Core>
-#include <QDebug>
+
 namespace hdps
 {
 
 class Texture2D : protected QOpenGLFunctions_3_3_Core
 {
 public:
-    Texture2D() : _handle(0)
+    Texture2D() : _handle(0) { }
+
+    void create()
     {
         initializeOpenGLFunctions();
 
         glGenTextures(1, &_handle);
     }
-
-    ~Texture2D()
-    {
-        glDeleteTextures(1, &_handle);
-    }
-
+    
     void bind()
     {
         glBindTexture(GL_TEXTURE_2D, _handle);
@@ -35,6 +32,16 @@ public:
     void release()
     {
         glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    void destroy()
+    {
+        glDeleteTextures(1, &_handle);
+    }
+
+    GLuint getHandle()
+    {
+        return _handle;
     }
 
 protected:
