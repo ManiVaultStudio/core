0bb75202944dea64ef5699a84723b155c210ecc8
Julian Thijssen <julianthijssen@gmail.com>
2018-06-08 17:02:31 +0200

    Remove gui namespace from Renderer

--- HDPS/src/renderers/Renderer.h
+++ HDPS/src/renderers/Renderer.h
@@ -13,14 +13,11 @@
 
 namespace hdps
 {
-    namespace gui
+    class Renderer : protected QOpenGLFunctions_3_3_Core
     {
-        class Renderer : protected QOpenGLFunctions_3_3_Core
-        {
-            virtual void init() = 0;
-            virtual void resize(QSize renderSize) = 0;
-            virtual void render() = 0;
-            virtual void destroy() = 0;
-        };
-    }
+        virtual void init() = 0;
+        virtual void resize(QSize renderSize) = 0;
+        virtual void render() = 0;
+        virtual void destroy() = 0;
+    };
 }
