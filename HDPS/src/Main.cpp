d095f3564dde05105ad5b8be37aa4326c5bafae6
Julian Thijssen <julianthijssen@gmail.com>
2018-08-09 12:29:36 +0200

    Replace tabs by spaces in Main.cpp

--- HDPS/src/Main.cpp
+++ HDPS/src/Main.cpp
@@ -3,28 +3,30 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("CGV TU Delft");
+    QCoreApplication::setOrganizationDomain("graphics.tudelft.nl");
+    QCoreApplication::setApplicationName("HDPS");
+    // Necessary to instantiate QWebEngine from a plugin
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
 
-	QCoreApplication::setOrganizationName("CGV TU Delft");
-	QCoreApplication::setOrganizationDomain("graphics.tudelft.nl");
-	QCoreApplication::setApplicationName("HDPS");
+    QApplication app(argc, argv);
 
     // Retina display support for Mac OS and X11:
     // AA_UseHighDpiPixmaps attribute is off by default in Qt 5.1 but will most
     // likely be on by default in a future release of Qt.
     app.setAttribute(Qt::AA_UseHighDpiPixmaps);
 
-	QIcon appIcon;
-	appIcon.addFile(":/Icons/AppIcon32");
-	appIcon.addFile(":/Icons/AppIcon64");
-	appIcon.addFile(":/Icons/AppIcon128");
-	appIcon.addFile(":/Icons/AppIcon256");
-	appIcon.addFile(":/Icons/AppIcon512");
-	appIcon.addFile(":/Icons/AppIcon1024");
+    QIcon appIcon;
+    appIcon.addFile(":/Icons/AppIcon32");
+    appIcon.addFile(":/Icons/AppIcon64");
+    appIcon.addFile(":/Icons/AppIcon128");
+    appIcon.addFile(":/Icons/AppIcon256");
+    appIcon.addFile(":/Icons/AppIcon512");
+    appIcon.addFile(":/Icons/AppIcon1024");
     app.setWindowIcon(appIcon);
 
     hdps::gui::MainWindow mainWindow;
-	mainWindow.show();
+    mainWindow.show();
 
-	return app.exec();
+    return app.exec();
 }
