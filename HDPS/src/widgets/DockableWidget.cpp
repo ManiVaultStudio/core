6460ea347e625ff3277465a35ab1999d8cdd09dc
Julian Thijssen <julianthijssen@gmail.com>
2018-08-15 15:49:37 +0200

    Generate a unique name for dockable widgets

--- HDPS/src/widgets/DockableWidget.cpp
+++ HDPS/src/widgets/DockableWidget.cpp
@@ -5,6 +5,7 @@
 #include <QTextStream>
 #include <QObject>
 #include <QAction>
+#include <QUuid>
 
 namespace hdps
 {
@@ -21,6 +22,9 @@ DockableWidget::DockableWidget(QWidget *parent) :
     _mainLayout = new QGridLayout(_mainWidget);
     _mainLayout->setMargin(0);
     setWidget(_mainWidget);
+
+    // Generate a unique name for the widget to let Qt identify it and store/retrieve its state
+    setObjectName(QString("Dockable Widget ") + QUuid::createUuid().toString());
 
     QObject::connect(toggleViewAction(), &QAction::toggled, this, &DockableWidget::setVisibility);
 }
