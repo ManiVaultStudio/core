86f5f482eba63f8da5e211903695a75b8c92ca21
Julian Thijssen <Nimthora@gmail.com>
2019-09-05 13:53:54 +0200

    Move all PointsPlugin function implementations to .cpp file

--- HDPS/src/plugins/PointsPlugin.cpp
+++ HDPS/src/plugins/PointsPlugin.cpp
@@ -6,7 +6,7 @@
 Q_PLUGIN_METADATA(IID "nl.tudelft.PointsPlugin")
 
 // =============================================================================
-// View
+// PointsPlugin
 // =============================================================================
 
 PointsPlugin::~PointsPlugin(void)
@@ -24,6 +24,16 @@ hdps::Set* PointsPlugin::createSet() const
     return new IndexSet(_core, getName());
 }
 
+unsigned int PointsPlugin::getNumPoints() const
+{
+    return _data.size() / _numDimensions;
+}
+
+unsigned int PointsPlugin::getNumDimensions() const
+{
+    return _numDimensions;
+}
+
 const std::vector<float>& PointsPlugin::getData() const
 {
     return _data;
@@ -34,14 +44,14 @@ const std::vector<QString>& PointsPlugin::getDimensionNames() const
     return _dimNames;
 }
 
-void PointsPlugin::setData(float* data, unsigned int numPoints, unsigned int numDimensions)
+void PointsPlugin::setData(const float* data, unsigned int numPoints, unsigned int numDimensions)
 {
     _data.resize(numPoints * numDimensions);
     _numDimensions = numDimensions;
 
     if (data == nullptr)
         return;
-    
+
     std::memcpy(_data.data(), data, sizeof(float) * numPoints * numDimensions);
 }
 
@@ -61,6 +71,34 @@ float& PointsPlugin::operator[](unsigned int index)
 {
     return _data[index];
 }
+
+// Temporary property metadata
+QVariant PointsPlugin::getProperty(const QString & name) const
+{
+    if (!hasProperty(name))
+        return QVariant();
+
+    return _properties[name];
+}
+
+void PointsPlugin::setProperty(const QString & name, const QVariant & value)
+{
+    _properties[name] = value;
+}
+
+bool PointsPlugin::hasProperty(const QString & name) const
+{
+    return _properties.contains(name);
+}
+
+QStringList PointsPlugin::propertyNames() const
+{
+    return _properties.keys();
+}
+
+// =============================================================================
+// IndexSet
+// =============================================================================
 
 hdps::Set* IndexSet::copy() const
 {
