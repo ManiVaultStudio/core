10ec637837806a00b4fe2a3f389babcfc215ad29
Julian Thijssen <Nimthora@gmail.com>
2019-11-15 12:09:56 +0100

    Set scalar effect to None when colors are given to point renderer

--- Scatterplot/src/ScatterplotWidget.cpp
+++ Scatterplot/src/ScatterplotWidget.cpp
@@ -147,6 +147,7 @@ void ScatterplotWidget::setScalars(const std::vector<float>& scalars)
 void ScatterplotWidget::setColors(const std::vector<Vector3f>& colors)
 {
     _pointRenderer.setColors(colors);
+    _pointRenderer.setScalarEffect(None);
 
     update();
 }
