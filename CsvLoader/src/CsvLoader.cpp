ManiVaultStudio/core

commit 7dd610efeb04d1a7e85226ef34cef4f4e55d7342

Use registry settings to store and restore last directory path CsvLoader

CsvLoader/src/CsvLoader.cpp
@@ -7,6 +7,7 @@
 #include <QtDebug>
 
 #include <QFileDialog>
+#include <QFileInfo>
 #include <QInputDialog>
 
 Q_PLUGIN_METADATA(IID "nl.tudelft.CsvLoader")
@@ -27,11 +28,20 @@ void CsvLoader::init()
 
 void CsvLoader::loadData()
 {
-    QString fileName = QFileDialog::getOpenFileName(Q_NULLPTR, "Load File", "", "CSV Files (*.csv *)");
-    
-    // Don't try to load a file if the dialog was cancelled or the file name is empty
-    if (fileName.isNull() || fileName.isEmpty())
-        return;
+    const auto fileName = []
+    {
+        QSettings settings(QString::fromLatin1("HDPS"), QString::fromLatin1("Plugins/CsvLoader"));
+        const QLatin1String directoryPathKey("directoryPath");
+        const QString directoryPath = settings.value(directoryPathKey).toString();
+        QString fileName = QFileDialog::getOpenFileName(Q_NULLPTR, "Load File", directoryPath, "CSV Files (*.csv *)");
+
+        // Don't try to load a file if the dialog was cancelled or the file name is empty
+        if (fileName.isNull() || fileName.isEmpty())
+            return fileName;
+
+        settings.setValue(directoryPathKey, QFileInfo(fileName).absolutePath());
+        return fileName;
+    }();
 
     qDebug() << "Loading CSV file: " << fileName;
     QFile file(fileName);

commit 7ab0f47bf7126acc4ee9b6a8363d573d1200733f

Fix Error ("Failed to load file") when CsvLoader file dialog is canceled
The previous commit, SHA-1 7dd610efeb04d1a7e85226ef34cef4f4e55d7342 introduced an Error message box, "Failed to load file at: Reason: File was not found at location", whenever the file dialog would be canceled.

Hereby fixed.

CsvLoader/src/CsvLoader.cpp
@@ -43,6 +43,11 @@ void CsvLoader::loadData()
         return fileName;
     }();
 
+    if (fileName.isEmpty())
+    {
+        return;
+    }
+
     qDebug() << "Loading CSV file: " << fileName;
     QFile file(fileName);
 