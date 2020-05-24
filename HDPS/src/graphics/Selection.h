94b8b7bbdf005c348ab5d785236efb787b088fbd
Niels Dekker <N.Dekker@lumc.nl>
2020-05-24 22:08:51 +0200

    Move small member functions of Vector2f and Selection to header file
    Moved the implementation of small member functions of Vector2f and Selection from their .cpp file to their .h file.
    
    Defaulted their default-constructors.
    
    Removed user-defined destructors, according to the C++ "Rule of Zero".
    
    Declared them `constexpr` when possible, and added compile-time unit tests.
    
    A significant performance improvement was observed.

--- HDPS/src/graphics/Selection.h
+++ HDPS/src/graphics/Selection.h
@@ -7,36 +7,83 @@ namespace hdps
     class Selection
     {
     public:
-        Selection();
+        // C++ Rule of Zero: Implicitly defined destructor and copy member functions are fine for this class.
+
+        Selection() = default;
+
         Selection(Vector2f start, Vector2f end);
-        ~Selection();
 
         void set(Vector2f start, Vector2f end);
 
         void setStart(Vector2f start);
         void setEnd(Vector2f end);
 
-        Vector2f getStart() const;
-        Vector2f getEnd() const;
-        Vector2f getCenter() const;
+        constexpr Vector2f getStart() const
+        {
+            return _start;
+        }
 
-        float getLeft() const;
-        float getRight() const;
-        float getBottom() const;
-        float getTop() const;
-        Vector2f topLeft() const;
-        Vector2f bottomLeft() const;
-        Vector2f bottomRight() const;
-        Vector2f topRight() const;
+        constexpr Vector2f getEnd() const
+        {
+            return _end;
+        }
 
-        bool contains(Vector2f point) const;
+        constexpr Vector2f getCenter() const
+        {
+            return Vector2f((_left + _right) / 2, (_bottom + _top) / 2);
+        }
+
+        constexpr float getLeft() const
+        {
+            return _left;
+        }
+
+        constexpr float getRight() const
+        {
+            return _right;
+        }
+
+        constexpr float getBottom() const
+        {
+            return _bottom;
+        }
+
+        constexpr float getTop() const
+        {
+            return _top;
+        }
+
+        constexpr Vector2f topLeft() const
+        {
+            return Vector2f(_left, _top);
+        }
+
+        constexpr Vector2f bottomLeft() const
+        {
+            return Vector2f(_left, _bottom);
+        }
+
+        constexpr Vector2f bottomRight() const
+        {
+            return Vector2f(_right, _bottom);
+        }
+
+        constexpr Vector2f topRight() const
+        {
+            return Vector2f(_right, _top);
+        }
+
+        constexpr bool contains(Vector2f point) const
+        {
+            return point.x >= _left && point.x <= _right && point.y >= _bottom && point.y <= _top;
+        }
 
     private:
         void updateProperties();
 
         Vector2f _start, _end;
 
-        float _left, _right, _bottom, _top;
+        float _left{}, _right{}, _bottom{}, _top{};
     };
 
 } // namespace hdps
