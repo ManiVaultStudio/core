190fb8af9381f2a3762a5a848282d6af84ce5dc9
B. van Lew <b.van_lew@lumc.nl>
2020-10-07 09:07:44 +0200

    Set shared option via profile

--- build.py
+++ build.py
@@ -4,13 +4,13 @@
 
 from cpt.packager import ConanMultiPackager
 import os
-def _is_not_shared(build):
-    return build.options['hdps-core:shared'] == False
+#def _is_not_shared(build):
+#    return build.options['hdps-core:shared'] == False
     
 if __name__ == "__main__":
 
     builder = ConanMultiPackager(reference="hdps-core/0.1.0")
     # builder.add_common_builds()
     builder.add()
-    builder.remove_build_if(_is_not_shared)  
+    # builder.remove_build_if(_is_not_shared)  
     builder.run('/tmp/conan/action_build')
