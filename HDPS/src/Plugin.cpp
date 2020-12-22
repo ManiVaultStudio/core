fef3eabf1433e57d234a24b59eb139e71c34b6c2
Thomas <t.kroes@lumc.nl>
2020-12-22 12:40:29 +0100

    Add GUI name functionality to plugin base class

--- HDPS/src/Plugin.cpp
+++ HDPS/src/Plugin.cpp
@@ -8,6 +8,17 @@ namespace hdps
 namespace plugin
 {
 
+QMap<QString, std::int32_t> hdps::plugin::Plugin::_noInstances = QMap<QString, std::int32_t>();
+
+Plugin::Plugin(Type type, QString kind) :
+    _name(kind + QUuid::createUuid().toString()),
+    _guiName(QString("%1 %2").arg(kind, QString::number(_noInstances[kind] + 1))),
+    _kind(kind),
+    _type(type)
+{
+    _noInstances[kind]++;
+}
+
 QVariant Plugin::getSetting(const QString& path, const QVariant& defaultValue /*= QVariant()*/) const
 {
     return Application::current()->getSetting(QString("%1/%2").arg(_kind, path), defaultValue);
