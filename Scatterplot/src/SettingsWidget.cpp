f7b7d75b745bf762705044cf606ab4007192d2a9
Thomas <t.kroes@lumc.nl>
2021-01-24 15:07:17 +0100

    Work on stateful widget

--- Scatterplot/src/SettingsWidget.cpp
+++ Scatterplot/src/SettingsWidget.cpp
@@ -17,7 +17,7 @@
 const hdps::Vector3f SettingsWidget::DEFAULT_BASE_COLOR = hdps::Vector3f(255.f / 255, 99.f / 255, 71.f / 255);
 const hdps::Vector3f SettingsWidget::DEFAULT_SELECTION_COLOR = hdps::Vector3f(72.f / 255, 61.f / 255, 139.f / 255);
 
-class StatefulWidget : public QWidget {
+class StatefulWidget : private QObject {
 public:
     enum class State {
         Popup,
@@ -25,30 +25,39 @@ public:
         Full
     };
 
-    StatefulWidget(const QString& name, const std::int32_t& priority = 0) :
-        QWidget(),
+    StatefulWidget(QWidget* widget, const QString& name, const std::int32_t& priority = 0) :
+        _widget(widget),
         _name(name),
         _priority(priority),
-        _invisibleWidget(new QWidget()),
-        _sizeHints({QSize(), QSize(), QSize()}),
-        _pushButton(new QPushButton())
+        _sizeHintWidget(new QWidget()),
+        _sizeHints({QSize(), QSize(), QSize()})
     {
-        setObjectName("StatefulWidget");
-        setStyleSheet("QWidget#StatefulWidget { background-color: green;}");
-        //setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        _widget->setObjectName("StatefulWidget");
+        _widget->setStyleSheet("QWidget#StatefulWidget { background-color: green;}");
+        //_widget->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    }
 
-        this->installEventFilter(this);
+    void initialize() {
+        _widget->installEventFilter(this);
 
         _sizeHints[0] = getStateSizeHint(State::Popup);
         _sizeHints[1] = getStateSizeHint(State::Compact);
         _sizeHints[2] = getStateSizeHint(State::Full);
 
-        setProperty("sizeHintMin", _sizeHints[0]);
-        setProperty("sizeHintMax", _sizeHints[2]);
+        _widget->setProperty("sizeHintMin", _sizeHints[0]);
+        _widget->setProperty("sizeHintMax", _sizeHints[2]);
 
         changeState(State::Popup);
     }
 
+    QSize sizeHint() const {
+        return QSize(20, 20);
+    };
+
+protected:
+    virtual QLayout* getLayout(const State& state) = 0;
+
+private:
     bool eventFilter(QObject* object, QEvent* event)
     {
         if (event->type() == QEvent::DynamicPropertyChange)
@@ -80,62 +89,44 @@ public:
 
     std::int32_t getAvailableWidth() const
     {
-        const auto availableWidthProperty = property("availableWidth");
+        const auto availableWidthProperty = _widget->property("availableWidth");
         return availableWidthProperty.toInt();
     }
 
     void changeState(const State& state) {
-        if (layout())
-            delete layout();
+        if (_widget->layout())
+            delete _widget->layout();
 
-        setLayout(getLayout(state));
-        update();
-    }
+        auto layout = getLayout(state);
 
-    QLayout* getLayout(const State& state) {
-        auto layout = new QHBoxLayout();
+        layout->setMargin(LAYOUT_MARGIN);
+        layout->setSpacing(LAYOUT_SPACING);
 
-        layout->addWidget(_pushButton);
-
-        switch (state)
-        {
-            case State::Popup:
-                _pushButton->setText("Popup");
-                _pushButton->setFixedWidth(50);
-                break;
-
-            case State::Compact:
-                _pushButton->setText("Compact");
-                _pushButton->setFixedWidth(150);
-                break;
-
-            case State::Full:
-                _pushButton->setText("Full");
-                _pushButton->setFixedWidth(300);
-
-                break;
-            default:
-                break;
-        }
-
-        return layout;
+        _widget->setLayout(layout);
     }
 
     QSize getStateSizeHint(const State& state) {
-        if (_invisibleWidget->layout())
-            delete _invisibleWidget->layout();
+        if (_sizeHintWidget->layout())
+            delete _sizeHintWidget->layout();
 
-        _invisibleWidget->setLayout(getLayout(state));
+        _sizeHintWidget->setLayout(getLayout(state));
 
-        return _invisibleWidget->sizeHint();
+        return _sizeHintWidget->sizeHint();
     }
 
+    QWidget*        _widget;
     QString         _name;
     std::int32_t    _priority;
-    QWidget*        _invisibleWidget;
+    QWidget*        _sizeHintWidget;
     QList<QSize>    _sizeHints;
-    QPushButton*    _pushButton;
+
+public:
+    static const std::int32_t LAYOUT_MARGIN;
+    static const std::int32_t LAYOUT_SPACING;
 };
+
+const std::int32_t StatefulWidget::LAYOUT_MARGIN = 0;
+const std::int32_t StatefulWidget::LAYOUT_SPACING = 4;
 
 class ToolBar : public QWidget {
 public:
@@ -145,26 +136,28 @@ public:
             QWidget(),
             _targetWidget(targetWidget)
         {
+            Q_ASSERT(targetWidget != nullptr);
+
             setObjectName("TargetWidget");
             setStyleSheet("QWidget#TargetWidget { background-color: red;}");
-            setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+            //setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Expanding);
+
+            //setMinimumSize(targetWidget->property("sizeHintMin").toSize());
+            //setMaximumSize(targetWidget->property("sizeHintMax").toSize());
 
             this->installEventFilter(this);
         }
 
         QSize sizeHint() const {
-            return _targetWidget->property("maxSizeHint").toSize();
+            return _targetWidget->property("sizeHintMax").toSize();
         };
 
         bool eventFilter(QObject* target, QEvent* event)
         {
             switch (event->type()) {
                 case QEvent::Resize:
-                {
-                    //qDebug() << _targetWidget->windowTitle() << static_cast<QResizeEvent*>(event)->size();
-                    _targetWidget->setProperty("availableWidth", width());
+                    //_targetWidget->setProperty("availableWidth", width());
                     break;
-                }
 
                 default:
                     break;
@@ -181,27 +174,51 @@ public:
         _mainLayout(new QVBoxLayout()),
         _proxyLayout(new QHBoxLayout()),
         _widgetLayout(new QHBoxLayout()),
-        _statefulWidgets()
+        _widgets(),
+        _spacerWidget(new QWidget())
     {
         setLayout(_mainLayout);
+        setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+
+        _mainLayout->setMargin(StatefulWidget::LAYOUT_MARGIN);
+        _mainLayout->setSpacing(StatefulWidget::LAYOUT_SPACING);
 
         _mainLayout->addLayout(_proxyLayout);
         _mainLayout->addLayout(_widgetLayout);
 
+        _proxyLayout->setMargin(0);
+        _proxyLayout->setSpacing(1);
+
+        _widgetLayout->setMargin(StatefulWidget::LAYOUT_MARGIN);
+        _widgetLayout->setSpacing(StatefulWidget::LAYOUT_SPACING);
+
+        _spacerWidget->setObjectName("SpacerWidget");
+        _spacerWidget->setStyleSheet("QWidget#SpacerWidget { background-color: yellow;}");
+        _spacerWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+
+        _widgetLayout->addWidget(_spacerWidget);
+
         this->installEventFilter(this);
     }
     
-    void addWidget(StatefulWidget* statefulWidget, const std::int32_t& stretch = 0) {
-        _proxyLayout->addWidget(new ProxyWidget(statefulWidget), stretch);
-        _widgetLayout->addWidget(statefulWidget);
+    void addWidget(QWidget* widget, const std::int32_t& stretch = 0) {
+        _widgets << widget;
+
+        _proxyLayout->addWidget(new ProxyWidget(widget), 1);
+        _widgetLayout->insertWidget(_widgetLayout->count() - 1, widget);
     }
 
     void addStretch(const std::int32_t& stretch = 1) {
-        _widgetLayout->addStretch(stretch);
+        //_widgetLayout->addStretch(stretch);
     }
 
-    bool eventFilter(QObject* target, QEvent* event)
+    bool eventFilter(QObject* object, QEvent* event)
     {
+        auto widget = dynamic_cast<QWidget*>(object);
+
+        if (widget != _spacerWidget)
+            return QObject::eventFilter(object, event);
+
         switch (event->type()) {
             case QEvent::Resize:
                 //qDebug() << "Toolbar:" << sizeHint();
@@ -211,16 +228,103 @@ public:
                 break;
         }
 
-        return QObject::eventFilter(target, event);
+        return QObject::eventFilter(object, event);
     }
 
-    QVBoxLayout*            _mainLayout;
-    QHBoxLayout*            _proxyLayout;
-    QHBoxLayout*            _widgetLayout;
-    QList<StatefulWidget*>  _statefulWidgets;
+    QSize sizeHint() const {
+        return QSize(20, 20);
+    };
+
+    QVBoxLayout*        _mainLayout;
+    QHBoxLayout*        _proxyLayout;
+    QHBoxLayout*        _widgetLayout;
+    QList<QWidget*>     _widgets;
+    QWidget*            _spacerWidget;
 };
 
-//class StatefulWidgetA : public QWidget, public StatefulWidget
+class StatefulWidgetA : public QWidget, public StatefulWidget {
+public:
+    StatefulWidgetA() :
+        QWidget(),
+        StatefulWidget(this, "A"),
+        _pushButton(new QPushButton)
+    {
+        initialize();
+    }
+
+    QLayout* getLayout(const State& state) override {
+        auto layout = new QHBoxLayout();
+
+        layout->addWidget(_pushButton);
+
+        switch (state)
+        {
+            case State::Popup:
+                _pushButton->setText("Popup");
+                _pushButton->setFixedWidth(50);
+                break;
+
+            case State::Compact:
+                _pushButton->setText("Compact");
+                _pushButton->setFixedWidth(100);
+                break;
+
+            case State::Full:
+                _pushButton->setText("Full");
+                _pushButton->setFixedWidth(150);
+
+                break;
+            default:
+                break;
+        }
+
+        return layout;
+    }
+
+    QPushButton*    _pushButton;
+};
+
+class StatefulWidgetB : public QWidget, public StatefulWidget {
+public:
+    StatefulWidgetB() :
+        QWidget(),
+        StatefulWidget(this, "B"),
+        _pushButton(new QPushButton)
+    {
+        initialize();
+    }
+
+    QLayout* getLayout(const State& state) override {
+        auto layout = new QHBoxLayout();
+
+        layout->addWidget(_pushButton);
+
+        switch (state)
+        {
+            case State::Popup:
+                _pushButton->setText("Popup");
+                _pushButton->setFixedWidth(50);
+                break;
+
+            case State::Compact:
+                _pushButton->setText("Compact");
+                _pushButton->setFixedWidth(400);
+                break;
+
+            case State::Full:
+                _pushButton->setText("Full");
+                _pushButton->setFixedWidth(500);
+
+                break;
+            default:
+                break;
+        }
+
+        return layout;
+    }
+
+    QPushButton*    _pushButton;
+};
 
 SettingsWidget::SettingsWidget(const ScatterplotPlugin& plugin) :
     QWidget(static_cast<QWidget*>(&const_cast<ScatterplotPlugin&>(plugin))),
@@ -305,8 +409,9 @@ SettingsWidget::SettingsWidget(const ScatterplotPlugin& plugin) :
 
     findDialog->layout()->addWidget(toolbar);
 
-    toolbar->addWidget(new StatefulWidget("A", 10));// , 1);
-    toolbar->addWidget(new StatefulWidget("B", 100));//, 4);
+    toolbar->addWidget(new StatefulWidgetA());
+    toolbar->addWidget(new StatefulWidgetB());
+    //toolbar->addWidget(new StatefulWidget("B", 100));//, 4);
     toolbar->addStretch();
 
     findDialog->show();
