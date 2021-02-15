fbcd3de11745e9e6b1d835ef53b6011e2a92e7f1
Julian Thijssen <Nimthora@gmail.com>
2021-02-15 14:58:30 +0100

    Add a little bit more documentation

--- HDPS/src/event/EventListener.h
+++ HDPS/src/event/EventListener.h
@@ -14,13 +14,17 @@ namespace hdps
         /** Event registration function signatures */
         using DataEventHandler = std::function<void(DataEvent)>;
 
+        // Disabled until a better solution can be found as currently it requires
+        // plugins to manually unregister if they are no longer interested in a dataset.
         //void registerDataEventByName(QString dataSetName, DataEventHandler callback);
 
         void registerDataEventByType(DataType dataType, DataEventHandler callback);
 
     protected:
+        // Sets internal static event core, called once upon plugin creation
         void setEventCore(CoreInterface* core);
 
+        // Unregisters the event listener from the core
         ~EventListener();
 
     private:
