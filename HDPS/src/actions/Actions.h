ff2a02de188f5763c2c599e8e7fc2539cce1a66a
Thomas <t.kroes@lumc.nl>
2021-06-14 13:40:11 +0200

    Fix faulty include

--- HDPS/src/actions/Actions.h
+++ HDPS/src/actions/Actions.h
@@ -4,7 +4,6 @@
 #include "DecimalAction.h"
 #include "OptionAction.h"
 #include "TriggerAction.h"
-#include "CheckAction.h"
 #include "ToggleAction.h"
 #include "StringAction.h"
 #include "WidgetAction.h"
