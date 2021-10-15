f187e216c6599cf458d924c99653780a0b853d27
Thomas Kroes <t.kroes@lumc.nl>
2021-10-15 11:31:12 +0200

    Small changes

--- HDPS/src/actions/GroupAction.cpp
+++ HDPS/src/actions/GroupAction.cpp
@@ -111,7 +111,7 @@ GroupAction::FormWidget::FormWidget(QWidget* parent, GroupAction* groupAction) :
 
     auto contentsMargin = _layout->contentsMargins();
     
-    _layout->setMargin(20);
+    _layout->setMargin(15);
 
     setLayout(_layout);
 
