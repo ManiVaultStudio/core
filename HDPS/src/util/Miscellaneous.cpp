c5dca5747e8c28ed1b00716834092e14781021ec
Thomas Kroes <t.kroes@lumc.nl>
2021-11-10 08:52:25 +0100

    Add function to obtain a human readable integer count

--- HDPS/src/util/Miscellaneous.cpp
+++ HDPS/src/util/Miscellaneous.cpp
@@ -7,6 +7,20 @@ namespace hdps
 namespace util
 {
 
+QString getIntegerCountHumanReadable(const float& count)
+{
+    if (count >= 0 && count < 1000)
+        return QString::number(count);
+
+    if (count >= 1000 && count < 1000000)
+        return QString("%1 k").arg(QString::number(count / 1000.0f, 'f', 1));
+
+    if (count >= 1000000)
+        return QString("%1 mln").arg(QString::number(count / 1000000.0f, 'f', 1));
+
+    return "";
+}
+
 QString getNoBytesHumanReadable(float noBytes)
 {
     QStringList list{ "KB", "MB", "GB", "TB" };
--- HDPS/src/util/Miscellaneous.h
+++ HDPS/src/util/Miscellaneous.h
@@ -7,6 +7,18 @@ namespace hdps
 namespace util
 {
 
+/**
+ * Returns a human readable string of an integer count
+ * @param count Integer count
+ * @return Human readable string of an integer count
+ */
+QString getIntegerCountHumanReadable(const float& count);
+
+/**
+ * Returns a human readable string of a byte count
+ * @param noBytes Number of bytes
+ * @return Human readable string of a byte count
+ */
 QString getNoBytesHumanReadable(float noBytes);
 
 }
