54adc19da758a7b0d95069d9cef1dee487c50129
Thomas Kroes <t.kroes@lumc.nl>
2022-10-30 09:56:27 +0100

    Add base hierarchy widget

---
+++ HDPS/src/widgets/HierarchyWidget.cpp
@@ -0,0 +1,163 @@
+#include "HierarchyWidget.h"
+#include "Application.h"
+
+#include <QDebug>
+#include <QVBoxLayout>
+#include <QSortFilterProxyModel>
+
+#include <stdexcept>
+
+using namespace hdps::util;
+
+namespace hdps
+{
+
+namespace gui
+{
+
+HierarchyWidget::HierarchyWidget(QWidget* parent, QAbstractItemModel& model, QSortFilterProxyModel* filterModel /*= nullptr*/) :
+    QWidget(parent),
+    _model(model),
+    _filterModel(filterModel),
+    _selectionModel(_filterModel != nullptr ? _filterModel : &_model),
+    _treeView(this),
+    _expandAllAction(this, "Expand all"),
+    _collapseAllAction(this, "Collapse all")
+{
+    if (_filterModel) {
+        _filterModel->setSourceModel(&_model);
+        _treeView.setModel(_filterModel);
+    }
+    else {
+        _treeView.setModel(&_model);
+    }
+
+    _treeView.setContextMenuPolicy(Qt::CustomContextMenu);
+    _treeView.setSelectionModel(&_selectionModel);
+    _treeView.setDragEnabled(true);
+    _treeView.setDragDropMode(QAbstractItemView::DragOnly);
+    _treeView.setSelectionBehavior(QAbstractItemView::SelectRows);
+    _treeView.setSelectionMode(QAbstractItemView::ExtendedSelection);
+    _treeView.setRootIsDecorated(true);
+    _treeView.setItemsExpandable(true);
+    _treeView.setIconSize(QSize(14, 14));
+
+    _expandAllAction.setIcon(Application::getIconFont("FontAwesome").getIcon("angle-double-down"));
+    _expandAllAction.setToolTip("Expand all datasets in the hierarchy");
+
+    _collapseAllAction.setIcon(Application::getIconFont("FontAwesome").getIcon("angle-double-up"));
+    _collapseAllAction.setToolTip("Collapse all datasets in the hierarchy");
+
+    connect(&_expandAllAction, &TriggerAction::triggered, this, [this]() -> void {
+        //if (!filterModelIndex.isValid())
+        //    return;
+
+        //_model.getItem(toSourceModelIndex(filterModelIndex), Qt::DisplayRole)->getDataHierarchyItem()->setExpanded(true);
+    });
+
+    connect(&_collapseAllAction, &TriggerAction::triggered, this, [this]() -> void {
+        //if (!filterModelIndex.isValid())
+        //    return;
+
+        //_model.getItem(_filterModel.mapToSource(filterModelIndex), Qt::DisplayRole)->getDataHierarchyItem()->setExpanded(false);
+    });
+
+    const auto numberOfRowsChanged = [this]() -> void {
+        const auto noItems = _model.rowCount() <= 0;
+
+        _expandAllAction.setEnabled(!noItems && mayExpandAll());
+        _collapseAllAction.setEnabled(!noItems && mayCollapseAll());
+
+        /*
+        const auto dataIsLoaded = _model.rowCount() >= 1;
+
+        _noDataOverlayWidget->setVisible(!dataIsLoaded);
+
+        _treeView.setHeaderHidden(!dataIsLoaded);
+        */
+    };
+
+    connect(&_model, &QAbstractItemModel::rowsInserted, this, numberOfRowsChanged);
+    connect(&_model, &QAbstractItemModel::rowsRemoved, this, numberOfRowsChanged);
+
+    numberOfRowsChanged();
+}
+
+QModelIndex HierarchyWidget::toSourceModelIndex(const QModelIndex& modelIndex) const
+{
+    if (_filterModel)
+        return _filterModel->mapToSource(modelIndex);
+
+    return modelIndex;
+}
+
+QModelIndexList HierarchyWidget::getSelectedRows() const
+{
+    QModelIndexList selectedRows;
+
+    for (const auto& index : _selectionModel.selectedRows())
+        selectedRows << toSourceModelIndex(index);
+
+    return selectedRows;
+}
+
+QModelIndexList HierarchyWidget::fetchFilterModelIndices(QModelIndex filterModelIndex /*= QModelIndex()*/) const
+{
+    QModelIndexList modelIndexList;
+
+    if (_filterModel == nullptr)
+        return modelIndexList;
+
+    if (filterModelIndex.isValid())
+        modelIndexList << filterModelIndex;
+
+    for (int rowIndex = 0; rowIndex < _filterModel->rowCount(filterModelIndex); ++rowIndex) {
+        QModelIndex index = _filterModel->index(rowIndex, 0, filterModelIndex);
+
+        if (_filterModel->hasChildren(index))
+            modelIndexList << fetchFilterModelIndices(index);
+    }
+
+    return modelIndexList;
+}
+
+bool HierarchyWidget::mayExpandAll() const
+{
+    const auto allFilterModelIndices = fetchFilterModelIndices();
+
+    for (const auto& filterModelIndex : allFilterModelIndices)
+        if (_model.rowCount(filterModelIndex) > 0 && !_treeView.isExpanded(filterModelIndex))
+            return true;
+
+    return false;
+}
+
+void HierarchyWidget::expandAll()
+{
+    const auto allFilterModelIndices = fetchFilterModelIndices();
+
+    for (const auto& filterModelIndex : allFilterModelIndices)
+        _treeView.setExpanded(filterModelIndex, true);
+}
+
+bool HierarchyWidget::mayCollapseAll() const
+{
+    const auto allFilterModelIndices = fetchFilterModelIndices();
+
+    for (const auto& filterModelIndex : allFilterModelIndices)
+        if (_model.rowCount(filterModelIndex) > 0 && _treeView.isExpanded(filterModelIndex))
+            return true;
+
+    return false;
+}
+
+void HierarchyWidget::collapseAll()
+{
+    const auto allFilterModelIndices = fetchFilterModelIndices();
+
+    for (const auto& filterModelIndex : allFilterModelIndices)
+        _treeView.setExpanded(filterModelIndex, false);
+}
+
+}
+}
---
+++ HDPS/src/widgets/HierarchyWidget.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include "actions/TriggerAction.h"
+
+#include <QWidget>
+#include <QTreeView>
+#include <QAbstractItemModel>
+
+class QSortFilterProxyModel;
+
+namespace hdps
+{
+
+namespace gui
+{
+
+/**
+ * Base widget for display a hierarchical model in a tree view
+ * It sets up the tree view and links up the filter and selection models
+ * Contains expand/collapse all actions
+ * 
+ * @author Thomas Kroes
+ */
+class HierarchyWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+
+    /**
+     * Constructor
+     * @param parent Parent widget
+     * @param model Reference to input model
+     * @param filterModel Pointer to input filter model (if present)
+     */
+    HierarchyWidget(QWidget* parent, QAbstractItemModel& model, QSortFilterProxyModel* filterModel = nullptr);
+
+    /**
+     * Get input model
+     * @return Input model
+     */
+    QAbstractItemModel& getModel() {
+        return _model;
+    }
+
+    /**
+     * Get input filter model
+     * @return Pointer to input filter model
+     */
+    QSortFilterProxyModel* getFilterModel() {
+        return _filterModel;
+    }
+
+    /**
+     * Get selection model
+     * @return Reference to selection model
+     */
+    QItemSelectionModel& getSelectionModel() {
+        return _selectionModel;
+    }
+
+    /**
+     * Get tree view widget
+     * @return Reference to tree view widget
+     */
+    QTreeView& getTreeView() {
+        return _treeView;
+    }
+
+    /**
+     * Get expand all action
+     * @return Reference to expand all action
+     */
+    TriggerAction& getExpandAllAction() {
+        return _expandAllAction;
+    }
+
+    /**
+     * Get collapse all action
+     * @return Reference to collapse all action
+     */
+    TriggerAction& getCollapseAllAction() {
+        return _collapseAllAction;
+    }
+
+    /**
+     * Maps a model index to source model index
+     * @param modelIndex Model index to map
+     * @return The mapped model index if a filter model is present, other wise the input model index
+     */
+    QModelIndex toSourceModelIndex(const QModelIndex& modelIndex) const;
+
+protected:
+
+    /**
+     * Get selected rows
+     * @return Model indices of the selected rows
+     */
+    QModelIndexList getSelectedRows() const;
+
+    /**
+     * Fetches filter model indices of \p filterModelIndex and its children recursively
+     * @param filterModelIndex Filter model index
+     * @return Fetched filter model indices
+     */
+    QModelIndexList fetchFilterModelIndices(QModelIndex filterModelIndex = QModelIndex()) const;
+
+protected: // Item expansion
+
+    /**
+     * Get whether one or more items may be expanded 
+     * @return Boolean indicating whether one or more items may be expanded
+     */
+    bool mayExpandAll() const;
+
+    /** Expand all items in the hierarchy */
+    void expandAll();
+
+    /**
+     * Get whether one or more items may be collapse
+     * @return Boolean indicating whether one or more items may be collapse
+     */
+    bool mayCollapseAll() const;
+
+    /** Collapse all items in the hierarchy */
+    void collapseAll();
+
+private:
+    QAbstractItemModel&         _model;                         /** Model containing data to be displayed in the hierarchy */
+    QSortFilterProxyModel*      _filterModel;                   /** Pointer to filter model (maybe nullptr) */
+    QItemSelectionModel         _selectionModel;                /** Selection model */
+    QTreeView                   _treeView;                      /** Tree view that contains the data hierarchy */
+    TriggerAction               _expandAllAction;               /** Expand all datasets action */
+    TriggerAction               _collapseAllAction;             /** Collapse all datasets action */
+    //NoDataOverlayWidget*        _noDataOverlayWidget;           /** Overlay help widget which is shown when no data is loaded */
+};
+
+}
+}
