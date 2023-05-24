ee4115b18139014e8c0b6e217af006218985776f
Alexander Vieth <a.vieth@tudelft.nl>
2023-05-24 08:55:32 +0200

    Uniform cmake options in top-level project

--- HDPS/cmake/CMakeCheckSetAVX.cmake
+++ HDPS/cmake/CMakeCheckSetAVX.cmake
@@ -2,25 +2,25 @@
 # Check for and link to AVX instruction sets
 # -----------------------------------------------------------------------------
 macro(check_and_set_AVX target useavx)
-	message(STATUS "Set instruction sets for ${target}, useavx is ${useavx}")
+	message(STATUS "Set instruction sets for ${target}, HDPS_USE_AVX is ${useavx}")
 	
-	# Use cmake hardware checks to see whether AVX should be activated
-	include(CheckCXXCompilerFlag)
-
-	if(MSVC)
-		set(AXV_CompileOption "/arch:AVX")
-		set(AXV2_CompileOption "/arch:AVX2")
-	else()
-		set(AXV_CompileOption "-DUSE_AVX")
-		set(AXV2_CompileOption "-DUSE_AVX2")
-	endif()
-	
-	if(NOT DEFINED COMPILER_OPT_AVX_SUPPORTED OR NOT DEFINED COMPILER_OPT_AVX2_SUPPORTED)
-		check_cxx_compiler_flag(${AXV_CompileOption} COMPILER_OPT_AVX_SUPPORTED)
-		check_cxx_compiler_flag(${AXV2_CompileOption} COMPILER_OPT_AVX2_SUPPORTED)
-	endif()
-
 	if(${useavx})
+		# Use cmake hardware checks to see whether AVX should be activated
+		include(CheckCXXCompilerFlag)
+
+		if(MSVC)
+			set(AXV_CompileOption "/arch:AVX")
+			set(AXV2_CompileOption "/arch:AVX2")
+		else()
+			set(AXV_CompileOption "-DUSE_AVX")
+			set(AXV2_CompileOption "-DUSE_AVX2")
+		endif()
+		
+		if(NOT DEFINED COMPILER_OPT_AVX_SUPPORTED OR NOT DEFINED COMPILER_OPT_AVX2_SUPPORTED)
+			check_cxx_compiler_flag(${AXV_CompileOption} COMPILER_OPT_AVX_SUPPORTED)
+			check_cxx_compiler_flag(${AXV2_CompileOption} COMPILER_OPT_AVX2_SUPPORTED)
+		endif()
+
 		if(COMPILER_OPT_AVX2_SUPPORTED)
 			message( STATUS "Use AXV2")
 			target_compile_options(${target} PRIVATE ${AXV2_CompileOption})
