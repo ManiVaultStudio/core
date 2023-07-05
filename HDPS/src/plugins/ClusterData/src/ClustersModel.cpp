fc66eed06766930551ccc2a3b8b843a11c2bee1c
Alexander Vieth <a.vieth@tudelft.nl>
2023-07-05 18:03:00 +0200

    Refer to LICENSE file in each file [skip ci]

--- HDPS/src/plugins/ClusterData/src/ClustersModel.cpp
+++ HDPS/src/plugins/ClusterData/src/ClustersModel.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later 
+// A corresponding LICENSE file is located in the root directory of this source tree 
 // Copyright (C) 2023 BioVault (Biomedical Visual Analytics Unit LUMC - TU Delft) 
 
 #include "ClustersModel.h"
