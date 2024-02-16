bd13f47297929f9f41a72927e4a4dfe4e4e5b4c4
Alexander Vieth <a.vieth@tudelft.nl>
2024-02-16 14:43:09 +0100

    remove bzip2

--- conanfile.py
+++ conanfile.py
@@ -52,7 +52,7 @@ class HdpsCoreConan(ConanFile):
     install_dir = None
     this_dir = os.path.dirname(os.path.realpath(__file__))
 
-    requires = ("qt/6.3.2@lkeb/stable", "bzip2/1.0.8@", "zlib/1.2.8@")
+    requires = ("qt/6.3.2@lkeb/stable", "zlib/1.2.8@")
 
     scm = {"type": "git", "subfolder": "hdps/core", "url": "auto", "revision": "auto"}
 
