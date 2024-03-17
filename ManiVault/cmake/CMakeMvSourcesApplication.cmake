5be2f955478989693333553a79c02e78d38119dc
Alexander Vieth <a.vieth@tudelft.nl>
2024-03-17 13:32:30 +0100

    Move resources to resource cmake

--- ManiVault/cmake/CMakeMvSourcesApplication.cmake
+++ ManiVault/cmake/CMakeMvSourcesApplication.cmake
@@ -8,14 +8,6 @@ set(MAIN_SOURCES
     res/styles/default.qss
 )
 
-if(WIN32)
-    set(MAIN_SOURCES ${MAIN_SOURCES} res/ResourcesWinIco.rc)
-endif(WIN32)
-
-if(APPLE)
-    set(MAIN_SOURCES ${MAIN_SOURCES} res/icons/AppIcon.icns)
-endif(APPLE)
-
 set(PRIVATE_CORE_HEADERS
     src/private/Core.h
 )
--- ManiVault/cmake/CMakeMvSourcesResources.cmake
+++ ManiVault/cmake/CMakeMvSourcesResources.cmake
@@ -1,17 +1,22 @@
 # -----------------------------------------------------------------------------
 # Resource files MV_EXE
 # -----------------------------------------------------------------------------
-# defines RESOURCE_FILES
+# defines RESOURCE_FILES and MAIN_APP_ICON_RESOURCE
 
 set(QRESOURCES
     res/ResourcesCore.qrc
 )
 
+if(WIN32)
+    set(MAIN_APP_ICON_RESOURCE ${MAIN_SOURCES} res/ResourcesWinIco.rc)
+    source_group(Resources FILES res/ResourcesWinIco.rc)
+endif(WIN32)
+
 if(APPLE)
+    set(MAIN_APP_ICON_RESOURCE ${MAIN_SOURCES} res/icons/AppIcon.icns)
     set_source_files_properties(res/icons/AppIcon.icns PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
 endif(APPLE)
 
-QT6_ADD_RESOURCES(RESOURCE_FILES ${QRESOURCES})
+qt_add_resources(RESOURCE_FILES ${QRESOURCES})
 
 source_group(Resources FILES ${QRESOURCES})
-source_group(Resources FILES res/ResourcesWinIco.rc)
