b46f878751923d3c18753775e12109a03c1cd203
Alexander Vieth <a.vieth@tudelft.nl>
2024-04-11 18:00:01 +0200

    Some debug prints

--- ManiVault/src/plugins/ClusterData/src/ClustersAction.cpp
+++ ManiVault/src/plugins/ClusterData/src/ClustersAction.cpp
@@ -87,20 +87,55 @@ void ClustersAction::updateClustersModel()
 
 void ClustersAction::updateClustersDataset()
 {
+    qDebug() << "ClustersAction::updateClustersDataset START";
+
+    qDebug() << "_clustersModel.getClusters";
+    qDebug() << _clustersModel.getClusters()[0].getColor();
+    qDebug() << "_clustersDataset->getClusters";
+    qDebug() << _clustersDataset->getClusters()[0].getColor();
+
     auto temp = _clustersModel.getClusters()[0].getColor();
 
     const auto& newClusters = _clustersModel.getClusters();
     const auto& currentClusters = _clustersDataset->getClusters();
 
     if (newClusters == currentClusters)
+    {
+        qDebug() << "ClustersAction::updateClustersDataset RETURN";
         return;
+    }
+
+    qDebug() << "ClustersAction::updateClustersDataset YES CHANGE";
 
     bool automaticRecolor = newClusters.size() != currentClusters.size();
 
     _clustersDataset->setClusters(newClusters);
 
+    qDebug() << "ClustersAction::updateClustersDataset MIDDLE 1";
+
+    qDebug() << "_clustersModel.getClusters";
+    qDebug() << _clustersModel.getClusters()[0].getColor();
+    qDebug() << "_clustersDataset->getClusters";
+    qDebug() << _clustersDataset->getClusters()[0].getColor();
+
     if (automaticRecolor)
         _colorizeClustersAction.updateColorsInModel();
 
+    qDebug() << "ClustersAction::updateClustersDataset MIDDLE 2";
+
+    qDebug() << "_clustersModel.getClusters";
+    qDebug() << _clustersModel.getClusters()[0].getColor();
+    qDebug() << "_clustersDataset->getClusters";
+    qDebug() << _clustersDataset->getClusters()[0].getColor();
+
     events().notifyDatasetDataChanged(_clustersDataset);
+
+    qDebug() << "ClustersAction::updateClustersDataset MIDDLE 3";
+
+    qDebug() << "_clustersModel.getClusters";
+    qDebug() << _clustersModel.getClusters()[0].getColor();
+    qDebug() << "_clustersDataset->getClusters";
+    qDebug() << _clustersDataset->getClusters()[0].getColor();
+
+    qDebug() << "ClustersAction::updateClustersDataset END";
 }
