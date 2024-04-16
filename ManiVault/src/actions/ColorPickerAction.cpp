0c07ec77ec19e0db936d7c83be7fa3c5dc6154f0
Alexander Vieth <a.vieth@tudelft.nl>
2024-04-16 14:55:51 +0200

    Use hslSaturation instead of saturation

--- ManiVault/src/actions/ColorPickerAction.cpp
+++ ManiVault/src/actions/ColorPickerAction.cpp
@@ -78,7 +78,7 @@ ColorPickerAction::Widget::Widget(QWidget* parent, ColorPickerAction* colorPicke
     _layout(),
     _colorDialog(),
     _hueAction(this, "Hue", 0, 359, colorPickerAction->getColor().hue()),
-    _saturationAction(this, "Saturation", 0, 255, colorPickerAction->getColor().saturation()),
+    _saturationAction(this, "Saturation", 0, 255, colorPickerAction->getColor().hslSaturation()),
     _lightnessAction(this, "Lightness", 0, 255, colorPickerAction->getColor().lightness()),
     _updateColorPickerAction(true)
 {
