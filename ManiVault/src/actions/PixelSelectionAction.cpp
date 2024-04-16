d2b75b597e82a58dd57db7c09a084ce867185a26
Thomas Kroes <t.kroes@lumc.nl>
2024-04-16 12:10:34 +0200

    Revert to replace mode if add or subtract modifier toggle actions are untoggled

--- ManiVault/src/actions/PixelSelectionAction.cpp
+++ ManiVault/src/actions/PixelSelectionAction.cpp
@@ -311,13 +311,11 @@ void PixelSelectionAction::initModifier()
     });
 
     connect(&_modifierAddAction, &QAction::toggled, [this](bool toggled) {
-        if (toggled)
-            _modifierAction.setCurrentIndex(static_cast<std::int32_t>(PixelSelectionModifierType::Add));
+        _modifierAction.setCurrentIndex(toggled ? static_cast<std::int32_t>(PixelSelectionModifierType::Add) : static_cast<std::int32_t>(PixelSelectionModifierType::Replace));
     });
 
     connect(&_modifierSubtractAction, &QAction::toggled, [this](bool toggled) {
-        if (toggled)
-            _modifierAction.setCurrentIndex(static_cast<std::int32_t>(PixelSelectionModifierType::Subtract));
+        _modifierAction.setCurrentIndex(toggled ? static_cast<std::int32_t>(PixelSelectionModifierType::Subtract) : static_cast<std::int32_t>(PixelSelectionModifierType::Replace));
     });
 
     if (!isInitialized())
