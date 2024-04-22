ManiVaultStudio/core

commit 9722ff5bb6b48c4c32d209595cb3bd42a9c5aac4

Add high-performance datasets model (#569)
* Add skeleton classes for high-performance datasets model

* Add skeleton code to CMake

* Work on AbstractDatasetsModel (compiles)

* DatasetPickerAction is working
Removed some debug prints

* Extra checks

* Extra checks

* Global actions list model

* Global actions list model

* Change actions list model to pointer in stead of a member

* Also fix in context menu

* Emit model changes when the dataset ID changes
Removed documented code
Implemented NameItem and LocationItem change handlers

* Emit datasetsChanged(...) whenever to model changes and the signal is not blocked
Implement private functions for blocking the datasetsChanged(...) signal

* Also respond to the QSortFilterProxyModel::dataChanged(...) signal
Keep a list of current datasets identifiers so that we can avoid unnecessary emits of the DatasetPickerAction::datasetsChanged(...) signal

* Add missing _datasetsListModel.setDatasets(...)

* enable edit dataset parameters

---------

Co-authored-by: Soumyadeep Basu <44787782+basusoumyadeep@users.noreply.github.com>

ManiVault/src/models/RawDataModel.cpp
@@ -123,6 +123,7 @@ QVariant RawDataModel::headerData(int section, Qt::Orientation orientation, int 
 
 void RawDataModel::populateFromDataManager()
 {
+    /*
 #ifdef RAW_DATA_MODEL_VERBOSE
     qDebug() << __FUNCTION__;
 #endif
@@ -134,6 +135,7 @@ void RawDataModel::populateFromDataManager()
 
     _countAction.setString(QString("Count: %1").arg(QString::number(rowCount())));
     _overallSizeAction.setString(QString("Overall size: %1").arg(util::getNoBytesHumanReadable(mv::data().getOverallRawDataSize())));
+    */
 }
 
 }

ManiVault/cmake/CMakeMvSourcesPublic.cmake
@@ -813,6 +813,23 @@ set(PUBLIC_MISCELLANEOUS_MODEL_FILES
     ${PUBLIC_MISCELLANEOUS_MODEL_SOURCES}
 )
 
+set(PUBLIC_DATASETS_MODEL_HEADERS
+    src/models/AbstractDatasetsModel.h
+    src/models/DatasetsListModel.h
+    src/models/DatasetsFilterModel.h
+)
+
+set(PUBLIC_DATASETS_MODEL_SOURCES
+    src/models/AbstractDatasetsModel.cpp
+    src/models/DatasetsListModel.cpp
+    src/models/DatasetsFilterModel.cpp
+)
+
+set(PUBLIC_DATASETS_MODEL_FILES
+    ${PUBLIC_DATASETS_MODEL_HEADERS}
+    ${PUBLIC_DATASETS_MODEL_SOURCES}
+)
+
 set(PUBLIC_GLOBAL_SETTINGS_HEADERS
     src/GlobalSettingsGroupAction.h
     src/ParametersSettingsAction.h
@@ -925,6 +942,7 @@ set(PUBLIC_HEADERS
     ${PUBLIC_PLUGINS_MODEL_HEADERS}
     ${PUBLIC_PLUGIN_FACTORIES_MODEL_HEADERS}
     ${PUBLIC_MISCELLANEOUS_MODEL_HEADERS}
+	${PUBLIC_DATASETS_MODEL_HEADERS}
     ${PUBLIC_GLOBAL_SETTINGS_HEADERS}
     ${PUBLIC_TASK_HEADERS}
     ${PUBLIC_VERSION_HEADERS}
@@ -967,6 +985,7 @@ set(PUBLIC_SOURCES
     ${PUBLIC_PLUGINS_MODEL_SOURCES}
     ${PUBLIC_PLUGIN_FACTORIES_MODEL_SOURCES}
     ${PUBLIC_MISCELLANEOUS_MODEL_SOURCES}
+	${PUBLIC_DATASETS_MODEL_SOURCES}
     ${PUBLIC_GLOBAL_SETTINGS_SOURCES}
     ${PUBLIC_TASK_SOURCES}
     ${PUBLIC_HEADERS}
@@ -1013,14 +1032,15 @@ source_group(Workspace FILES ${PUBLIC_WORKSPACE_FILES})
 source_group(DataHierarchy FILES ${PUBLIC_DATA_HIERARCHY_FILES})
 source_group(Dataset FILES ${PUBLIC_DATASET_FILES})
 source_group(Plugin FILES ${PUBLIC_PLUGIN_FILES})
-source_group(Model\\Actions FILES ${PUBLIC_ACTIONS_MODEL_FILES})
-source_group(Model\\DataHierarchy FILES ${PUBLIC_DATA_HIERARCHY_MODEL_FILES})
-source_group(Model\\RawData FILES ${PUBLIC_RAW_DATA_MODEL_FILES})
-source_group(Model\\Selections FILES ${PUBLIC_SELECTIONS_MODEL_FILES})
-source_group(Model\\Logging FILES ${PUBLIC_LOGGING_MODEL_FILES})
-source_group(Model\\Tasks FILES ${PUBLIC_TASKS_MODEL_FILES})
-source_group(Model\\Plugins FILES ${PUBLIC_PLUGINS_MODEL_FILES})
-source_group(Model\\PluginFactories FILES ${PUBLIC_PLUGIN_FACTORIES_MODEL_FILES})
-source_group(Model\\Miscellaneous FILES ${PUBLIC_MISCELLANEOUS_MODEL_FILES})
+source_group(Models\\Actions FILES ${PUBLIC_ACTIONS_MODEL_FILES})
+source_group(Models\\DataHierarchy FILES ${PUBLIC_DATA_HIERARCHY_MODEL_FILES})
+source_group(Models\\RawData FILES ${PUBLIC_RAW_DATA_MODEL_FILES})
+source_group(Models\\Selections FILES ${PUBLIC_SELECTIONS_MODEL_FILES})
+source_group(Models\\Logging FILES ${PUBLIC_LOGGING_MODEL_FILES})
+source_group(Models\\Tasks FILES ${PUBLIC_TASKS_MODEL_FILES})
+source_group(Models\\Plugins FILES ${PUBLIC_PLUGINS_MODEL_FILES})
+source_group(Models\\PluginFactories FILES ${PUBLIC_PLUGIN_FACTORIES_MODEL_FILES})
+source_group(Models\\Miscellaneous FILES ${PUBLIC_MISCELLANEOUS_MODEL_FILES})
+source_group(Models\\Datasets FILES ${PUBLIC_DATASETS_MODEL_FILES})
 source_group(GlobalSettings FILES ${PUBLIC_GLOBAL_SETTINGS_FILES})
 source_group(Task FILES ${PUBLIC_TASK_FILES})

ManiVault/src/AbstractActionsManager.h
@@ -18,6 +18,8 @@
 namespace mv
 {
 
+class ActionsListModel;
+
 /**
  * Abstract actions manager
  *
@@ -479,6 +481,10 @@ public: // Action types
         return actionTypesHumanFriendly;
     }
 
+public: // Models
+
+    virtual ActionsListModel& getActionsListModel() = 0;
+
 protected:
 
     /**
@@ -560,7 +566,7 @@ protected:
     gui::WidgetActions          _actions;               /** List of actions that are instantiated in the plugin system */
     gui::WidgetActions          _publicActions;         /** List of public actions that are instantiated in the plugin system */
     QMap<QString, ActionType>   _actionTypes;           /** Maps action type to counter and human-friendly action type string */
-
+    
     friend class gui::WidgetAction;
 };
 

ManiVault/src/AbstractDataManager.h
@@ -9,10 +9,13 @@
 
 #include "actions/ToggleAction.h"
 
+class QSortFilterProxyModel;
+
 namespace mv
 {
 
 class DatasetImpl;
+class DatasetsListModel;
 
 namespace plugin {
     class RawData;
@@ -315,6 +318,20 @@ public: // Dataset and selection grouping
      * @return Reference to selection grouping toggle action
      */
     virtual gui::ToggleAction& getSelectionGroupingAction() = 0;
+
+public: // Model
+
+    /**
+     * Get datasets list model
+     * @return Reference to datasets list model
+     */
+    virtual const DatasetsListModel& getDatasetsListModel() const = 0;
+
+    /**
+     * Link \p filterModel to the datasets list model
+     * @param filterModel Pointer to filter model
+     */
+    virtual void linkFilterModelToDatasetsListModel(QSortFilterProxyModel* filterModel) = 0;
 
 signals:
 

ManiVault/src/Plugin.cpp
@@ -6,7 +6,7 @@
 #include "CoreInterface.h"
 
 #ifdef _DEBUG
-    #define PLUGIN_VERBOSE
+    //#define PLUGIN_VERBOSE
 #endif
 
 using namespace mv::gui;

ManiVault/src/RawData.h
@@ -51,9 +51,9 @@ public:
      * @return Size of the raw data in bytes
      */
     virtual std::uint64_t getRawDataSize() const {
-#ifdef _DEBUG
-        qDebug() << __FUNCTION__ << "not implemented in derived class";
-#endif
+//#ifdef _DEBUG
+//        qDebug() << __FUNCTION__ << "not implemented in derived class";
+//#endif
 
         return {};
     }

ManiVault/src/Set.cpp
@@ -364,9 +364,6 @@ DatasetImpl::DatasetImpl(const QString& rawDataName, bool mayUnderive /*= true*/
 
 DatasetImpl::~DatasetImpl()
 {
-#ifdef _DEBUG
-    qDebug() << "Removed dataset" << getGuiName();
-#endif
 }
 
 void DatasetImpl::init()

ManiVault/src/actions/DatasetPickerAction.cpp
@@ -18,93 +18,112 @@ using namespace mv::util;
 
 namespace mv::gui {
 
-DatasetPickerAction::DatasetPickerAction(QObject* parent, const QString& title, Mode mode /*= Mode::Automatic*/) :
+DatasetPickerAction::DatasetPickerAction(QObject* parent, const QString& title) :
     OptionAction(parent, title),
-    _mode(mode),
-    _datasetsFilterFunction(),
-    _datasetsModel(),
-    _eventListener()
+    _populationMode(AbstractDatasetsModel::PopulationMode::Automatic),
+    _datasetsListModel(AbstractDatasetsModel::PopulationMode::Manual),
+    _datasetsFilterModel(),
+    _blockDatasetsChangedSignal(false),
+    _currentDatasetsIds()
 {
     setText(title);
     setIconByName("database");
     setToolTip("Pick a dataset");
-    setCustomModel(&_datasetsModel);
     setPlaceHolderString("--choose dataset--");
+    setCustomModel(&_datasetsFilterModel);
 
     connect(this, &OptionAction::currentIndexChanged, this, [this](const std::int32_t& currentIndex) {
-        emit datasetPicked(_datasetsModel.getDataset(currentIndex));
-    });
+        const auto sourceModelRow = _datasetsFilterModel.mapToSource(_datasetsFilterModel.index(currentIndex, 0)).row();
 
-    _eventListener.addSupportedEventType(static_cast<std::uint32_t>(EventType::DatasetAdded));
-    _eventListener.registerDataEvent([this](DatasetEvent* dataEvent) {
-        switch (dataEvent->getType()) {
-            case EventType::DatasetAdded:
-                populateDatasetsFromCore();
+        switch (_populationMode)
+        {
+            case AbstractDatasetsModel::PopulationMode::Manual:
+                emit datasetPicked(_datasetsListModel.getDataset(sourceModelRow));
+                break;
+
+            case AbstractDatasetsModel::PopulationMode::Automatic:
+                emit datasetPicked(mv::data().getDatasetsListModel().getDataset(sourceModelRow));
+                break;
+
+            default:
                 break;
         }
     });
 
-    switch (_mode)
-    {
-        case DatasetPickerAction::Mode::Manual:
-            break;
+    const auto filterModelChanged = [this]() -> void {
+        if (isDatasetsChangedSignalBlocked())
+            return;
 
-        case DatasetPickerAction::Mode::Automatic:
-            populateDatasetsFromCore();
-            break;
-        
-        default:
-            break;
-    }
+        const auto datasets = getDatasets();
+
+        QStringList datasetsIds;
+
+        for (const auto& dataset : datasets)
+            datasetsIds << dataset->getId();
+
+        if (datasetsIds == _currentDatasetsIds)
+            return;
+
+        _currentDatasetsIds = datasetsIds;
+
+        emit datasetsChanged(datasets);
+    };
+
+    connect(&_datasetsFilterModel, &QSortFilterProxyModel::rowsInserted, this, filterModelChanged);
+    connect(&_datasetsFilterModel, &QSortFilterProxyModel::rowsRemoved, this, filterModelChanged);
+    connect(&_datasetsFilterModel, &QSortFilterProxyModel::dataChanged, this, filterModelChanged);
+    connect(&_datasetsFilterModel, &QSortFilterProxyModel::layoutChanged, this, filterModelChanged);
+
+    populationModeChanged();
 }
 
-DatasetPickerAction::Mode DatasetPickerAction::getMode() const
+Datasets DatasetPickerAction::getDatasets() const
 {
-    return _mode;
+    Datasets datasets;
+
+    for (std::int32_t filterModelRowIndex = 0; filterModelRowIndex < _datasetsFilterModel.rowCount(); ++filterModelRowIndex) {
+        const auto sourceModelIndex = _datasetsFilterModel.mapToSource(_datasetsFilterModel.index(filterModelRowIndex, 0));
+
+        if (!sourceModelIndex.isValid())
+            continue;
+
+        switch (_populationMode)
+        {
+            case AbstractDatasetsModel::PopulationMode::Manual:
+                datasets << _datasetsListModel.getDataset(sourceModelIndex.row());
+                break;
+
+            case AbstractDatasetsModel::PopulationMode::Automatic:
+                datasets << mv::data().getDatasetsListModel().getDataset(sourceModelIndex.row());
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return datasets;
 }
 
-void DatasetPickerAction::setMode(Mode mode)
+void DatasetPickerAction::setDatasets(Datasets datasets, bool silent /*= false*/)
 {
 #ifdef DATASET_PICKER_ACTION_VERBOSE
     qDebug() << __FUNCTION__;
 #endif
 
-    if (mode == _mode)
-        return;
+    setPopulationMode(AbstractDatasetsModel::PopulationMode::Manual);
 
-    _mode = mode;
-
-    switch (_mode)
-    {
-        case DatasetPickerAction::Mode::Manual:
-            break;
-
-        case DatasetPickerAction::Mode::Automatic:
-            populateDatasetsFromCore();
-            break;
-
-        default:
-            break;
+    if (silent) {
+        blockDatasetsChangedSignal();
+        {
+            _datasetsListModel.setDatasets(datasets);
+        }
+        unblockDatasetsChangedSignal();
     }
-}
+    else {
+        _datasetsListModel.setDatasets(datasets);
 
-void DatasetPickerAction::setDatasets(Datasets datasets)
-{
-#ifdef DATASET_PICKER_ACTION_VERBOSE
-    qDebug() << __FUNCTION__;
-#endif
-
-    _mode = Mode::Manual;
-
-    _datasetsModel.setDatasets(datasets);
-
-    for (auto& dataset : _datasetsModel.getDatasets()) {
-
-        connect(&dataset, &Dataset<DatasetImpl>::aboutToBeRemoved, this, [this, dataset]() {
-            _datasetsModel.removeDataset(dataset);
-        });
-
-        connect(dataset.get(), &DatasetImpl::locationChanged, &_datasetsModel, &DatasetsModel::updateData);
+        emit datasetsChanged(_datasetsListModel.getDatasets());
     }
 
     auto publicDatasetPickerAction = dynamic_cast<DatasetPickerAction*>(getPublicAction());
@@ -113,74 +132,161 @@ void DatasetPickerAction::setDatasets(Datasets datasets)
         setCurrentDataset(publicDatasetPickerAction->getCurrentDataset());
 }
 
-void DatasetPickerAction::setDatasetsFilterFunction(const DatasetsFilterFunction& datasetsFilterFunction)
+void DatasetPickerAction::setFilterFunction(const DatasetsFilterModel::FilterFunction& filterFunction)
 {
 #ifdef DATASET_PICKER_ACTION_VERBOSE
     qDebug() << __FUNCTION__;
 #endif
 
-    _mode                   = Mode::Automatic;
-    _datasetsFilterFunction = datasetsFilterFunction;
-
-    populateDatasetsFromCore();
+    _datasetsFilterModel.setFilterFunction(filterFunction);
 }
 
 Dataset<DatasetImpl> DatasetPickerAction::getCurrentDataset() const
 {
-    return _datasetsModel.getDataset(getCurrentIndex());
+    if (getCurrentIndex() < 0)
+        return {};
+
+    const auto filterModelIndex = _datasetsFilterModel.index(getCurrentIndex(), 0);
+
+    if (!filterModelIndex.isValid())
+        return {};
+
+    const auto sourceModelIndex = _datasetsFilterModel.mapToSource(filterModelIndex);
+
+    if (!sourceModelIndex.isValid())
+        return {};
+
+    switch (_populationMode)
+    {
+        case AbstractDatasetsModel::PopulationMode::Manual:
+            return _datasetsListModel.getDataset(sourceModelIndex.row());
+
+        case AbstractDatasetsModel::PopulationMode::Automatic:
+            return mv::data().getDatasetsListModel().getDataset(sourceModelIndex.row());
+
+        default:
+            break;
+    }
+
+    return {};
 }
 
 void DatasetPickerAction::setCurrentDataset(Dataset<DatasetImpl> currentDataset)
 {
-    const auto currentIndex = _datasetsModel.rowIndex(currentDataset);
+    QModelIndex datasetIndex;
 
-    if (currentIndex < 0)
+    if (currentDataset.isValid()) {
+        switch (_populationMode)
+        {
+            case AbstractDatasetsModel::PopulationMode::Manual:
+                datasetIndex = _datasetsListModel.getIndexFromDataset(currentDataset);
+                break;
+
+            case AbstractDatasetsModel::PopulationMode::Automatic:
+                datasetIndex = mv::data().getDatasetsListModel().getIndexFromDataset(currentDataset);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if (!datasetIndex.isValid())
         return;
 
-    setCurrentIndex(currentIndex);
+    setCurrentIndex(_datasetsFilterModel.mapFromSource(datasetIndex).row());
 }
 
-void DatasetPickerAction::setCurrentDataset(const QString& guid)
+void DatasetPickerAction::setCurrentDataset(const QString& datasetId)
 {
-    if (guid.isEmpty())
+    if (datasetId.isEmpty())
         return;
 
-    const auto matches = _datasetsModel.match(_datasetsModel.index(0, static_cast<int>(DatasetsModel::Column::GUID)), Qt::EditRole, guid, -1);
+    QModelIndex datasetIndex;
 
-    if (matches.isEmpty())
+    switch (_populationMode)
+    {
+        case AbstractDatasetsModel::PopulationMode::Manual:
+            datasetIndex = _datasetsListModel.getIndexFromDataset(datasetId);
+            break;
+
+        case AbstractDatasetsModel::PopulationMode::Automatic:
+            datasetIndex = mv::data().getDatasetsListModel().getIndexFromDataset(datasetId);
+            break;
+
+        default:
+            break;
+    }
+
+    if (!datasetIndex.isValid())
         return;
 
-    setCurrentIndex(matches.first().row());
+    setCurrentIndex(_datasetsFilterModel.mapFromSource(datasetIndex).row());
 }
 
-QString DatasetPickerAction::getCurrentDatasetGuid() const
+QString DatasetPickerAction::getCurrentDatasetId() const
 {
     return getCurrentDataset().getDatasetId();
 }
 
-void DatasetPickerAction::populateDatasetsFromCore()
+AbstractDatasetsModel::PopulationMode DatasetPickerAction::getPopulationMode() const
 {
-    if (_mode == Mode::Manual)
-        return;
+    return _populationMode;
+}
 
+void DatasetPickerAction::setPopulationMode(AbstractDatasetsModel::PopulationMode populationMode)
+{
 #ifdef DATASET_PICKER_ACTION_VERBOSE
     qDebug() << __FUNCTION__;
 #endif
 
-    auto datasets = mv::data().getAllDatasets();
+    const auto previousPopulationMode = _populationMode;
 
-    if (_datasetsFilterFunction)
-        datasets = _datasetsFilterFunction(datasets);
+    if (populationMode == _populationMode)
+        return;
 
-    _datasetsModel.setDatasets(datasets);
+    _populationMode = populationMode;
 
-    for (auto& dataset : datasets)
-        connect(dataset.get(), &DatasetImpl::locationChanged, &_datasetsModel, &DatasetsModel::updateData);
+    populationModeChanged();
 
-    auto publicDatasetPickerAction = dynamic_cast<DatasetPickerAction*>(getPublicAction());
+    emit populationModeChanged(previousPopulationMode, _populationMode);
+}
 
-    if (publicDatasetPickerAction)
-        setCurrentDataset(publicDatasetPickerAction->getCurrentDataset());
+void DatasetPickerAction::populationModeChanged()
+{
+    switch (_populationMode)
+    {
+        case AbstractDatasetsModel::PopulationMode::Manual: {
+            _datasetsFilterModel.getUseFilterFunctionAction().setChecked(false);
+            _datasetsFilterModel.setSourceModel(&_datasetsListModel);
+            break;
+        }
+
+        case AbstractDatasetsModel::PopulationMode::Automatic:
+        {
+            _datasetsFilterModel.getUseFilterFunctionAction().setChecked(true);
+            _datasetsFilterModel.setSourceModel(&const_cast<DatasetsListModel&>(mv::data().getDatasetsListModel()));
+            break;
+        }
+
+        default:
+            break;
+    }
+}
+
+void DatasetPickerAction::blockDatasetsChangedSignal()
+{
+    _blockDatasetsChangedSignal = true;
+}
+
+void DatasetPickerAction::unblockDatasetsChangedSignal()
+{
+    _blockDatasetsChangedSignal = false;
+}
+
+bool DatasetPickerAction::isDatasetsChangedSignalBlocked() const
+{
+    return _blockDatasetsChangedSignal;
 }
 
 void DatasetPickerAction::connectToPublicAction(WidgetAction* publicAction, bool recursive)
@@ -232,196 +338,10 @@ QVariantMap DatasetPickerAction::toVariantMap() const
     QVariantMap variantMap = WidgetAction::toVariantMap();
 
     variantMap.insert({
-        { "Value", getCurrentDatasetGuid() }
+        { "Value", getCurrentDatasetId() }
     });
 
     return variantMap;
-}
-
-mv::Datasets DatasetPickerAction::getDatasets() const
-{
-    return _datasetsModel.getDatasets();
-}
-
-DatasetPickerAction::DatasetsModel::DatasetsModel(QObject* parent /*= nullptr*/) :
-    QAbstractListModel(parent),
-    _datasets(),
-    _showLocation(true),
-    _showIcon(true)
-{
-}
-
-int DatasetPickerAction::DatasetsModel::rowCount(const QModelIndex& parent /*= QModelIndex()*/) const
-{
-    return _datasets.count();
-}
-
-int DatasetPickerAction::DatasetsModel::rowIndex(const Dataset<DatasetImpl>& dataset) const
-{
-    return _datasets.indexOf(dataset);
-}
-
-int DatasetPickerAction::DatasetsModel::columnCount(const QModelIndex& parent /*= QModelIndex()*/) const
-{
-    return 2;
-}
-
-QVariant DatasetPickerAction::DatasetsModel::data(const QModelIndex& index, int role) const
-{
-    if (!index.isValid())
-        return QVariant();
-
-    const auto column   = static_cast<Column>(index.column());
-    const auto dataset  = _datasets.at(index.row());
-
-    if (!dataset.isValid())
-        return QVariant();
-
-    switch (role)
-    {
-        case Qt::DecorationRole:
-            return _showIcon ? dataset->getIcon() : QVariant();
-
-        case Qt::EditRole:
-        case Qt::DisplayRole:
-        {
-            switch (column)
-            {
-                case Column::Name:
-                    return _showLocation ? dataset->getLocation() : dataset->text();
-
-                case Column::GUID:
-                    return dataset->getId();
-
-                default:
-                    break;
-            }
-            break;
-        }
-
-        default:
-            break;
-    }
-
-    return QVariant();
-}
-
-const QVector<Dataset<DatasetImpl>>& DatasetPickerAction::DatasetsModel::getDatasets() const
-{
-    return _datasets;
-}
-
-Dataset<DatasetImpl> DatasetPickerAction::DatasetsModel::getDataset(const std::int32_t& rowIndex) const
-{
-    if (rowIndex < 0)
-        return Dataset<DatasetImpl>();
-
-    return _datasets[rowIndex];
-}
-
-void DatasetPickerAction::DatasetsModel::setDatasets(const QVector<Dataset<DatasetImpl>>& datasets)
-{
-    removeAllDatasets();
-
-    for (const auto& dataset : datasets)
-        addDataset(dataset);
-}
-
-void DatasetPickerAction::DatasetsModel::addDataset(const Dataset<DatasetImpl>& dataset)
-{
-    beginInsertRows(QModelIndex(), rowCount(), rowCount());
-    {
-        _datasets << dataset;
-    }
-    endInsertRows();
-
-    auto& addedDataset = _datasets.last();
-
-    connect(&addedDataset, &Dataset<DatasetImpl>::aboutToBeRemoved, this, [this, &addedDataset]() {
-        removeDataset(addedDataset);
-    });
-
-    connect(addedDataset.get(), &DatasetImpl::locationChanged, this, [this, &addedDataset]() {
-        const auto colorDatasetRowIndex = rowIndex(addedDataset);
-
-        if (colorDatasetRowIndex < 0)
-            return;
-
-        const auto modelIndex = index(colorDatasetRowIndex, 0);
-
-        if (!modelIndex.isValid())
-            return;
-
-        emit dataChanged(modelIndex, modelIndex);
-    });
-}
-
-void DatasetPickerAction::DatasetsModel::removeDataset(const mv::Dataset<mv::DatasetImpl>& dataset)
-{
-    const auto datasetRowIndex = rowIndex(dataset);
-
-    beginRemoveRows(QModelIndex(), datasetRowIndex, datasetRowIndex);
-    {
-        _datasets.removeOne(dataset);
-    }
-    endRemoveRows();
-}
-
-void DatasetPickerAction::DatasetsModel::removeAllDatasets()
-{
-    if (rowCount() == 0)
-        return;
-
-    beginRemoveRows(QModelIndex(), 0, std::max(0, rowCount() - 1));
-    {
-        _datasets.clear();
-    }
-    endRemoveRows();
-
-    updateData();
-}
-
-bool DatasetPickerAction::DatasetsModel::getShowIcon() const
-{
-    return _showIcon;
-}
-
-void DatasetPickerAction::DatasetsModel::setShowIcon(bool showIcon)
-{
-    if (showIcon == _showIcon)
-        return;
-
-    _showIcon = showIcon;
-
-    updateData();
-}
-
-bool DatasetPickerAction::DatasetsModel::getShowLocation() const
-{
-    return _showLocation;
-}
-
-void DatasetPickerAction::DatasetsModel::setShowLocation(bool showLocation)
-{
-    if (showLocation == _showLocation)
-        return;
-
-    _showLocation = showLocation;
-
-    updateData();
-}
-
-void DatasetPickerAction::DatasetsModel::updateData()
-{
-    for (auto dataset : _datasets) {
-
-        if (!dataset.isValid())
-            continue;
-
-        const auto datasetModelIndex = index(_datasets.indexOf(dataset), 0);
-
-        emit dataChanged(datasetModelIndex, datasetModelIndex);
-    }
 }
 
 }

commit cda01d3fbb9b02b226c0e71ecd5ca7c5259db108

Enable RawDataModel::populateFromDataManager again
Regression after #569

ManiVault/src/models/RawDataModel.cpp
@@ -123,7 +123,6 @@ QVariant RawDataModel::headerData(int section, Qt::Orientation orientation, int 
 
 void RawDataModel::populateFromDataManager()
 {
-    /*
 #ifdef RAW_DATA_MODEL_VERBOSE
     qDebug() << __FUNCTION__;
 #endif
@@ -135,7 +134,6 @@ void RawDataModel::populateFromDataManager()
 
     _countAction.setString(QString("Count: %1").arg(QString::number(rowCount())));
     _overallSizeAction.setString(QString("Overall size: %1").arg(util::getNoBytesHumanReadable(mv::data().getOverallRawDataSize())));
-    */
 }
 
 }