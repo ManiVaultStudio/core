d4819966b261c2b45155e358e771a7307a379b01
Baldur van Lew <baldurvanlew@gmail.com>
2024-05-01 12:54:02 +0200

    File path quoting for space in product name

--- ManiVault/macos_webengineinstall.sh
+++ ManiVault/macos_webengineinstall.sh
@@ -3,15 +3,16 @@
 qtroot=$1
 bundledir=$2
 
+echo "Bundle dir is " $bundledir
 
-rsync -a --delete $qtroot/../../QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/ $bundledir/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/
+rsync -a --delete $qtroot/../../QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/ "$bundledir/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app"/
 
-if ! [[ -f $bundledir/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess ]]; then
+if ! [[ -f "$bundledir/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess" ]]; then
     echo "No QtWebEngineProcess.app not found. Was macdeployqt successful?"
     exit 1
 fi
 
-pushd $bundledir/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS
+pushd "$bundledir/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS"
 for LIB in QtGui QtCore QtWebEngineCore QtQuick QtWebChannel QtNetwork QtPositioning QtQmlModels QtQml
 do
     OLD_PATH=`otool -L QtWebEngineProcess | grep ${LIB}.framework | cut -f 1 -d ' '`
