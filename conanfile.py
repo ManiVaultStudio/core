9502242eee6fe693387775a5c6c9ec252dfb1930
Baldur van Lew <b.van_lew@lumc.nl>
2024-05-22 11:43:58 +0200

    [skip ci] Update conanfile.py with compatibility
    Add compatibility fallback for zlib

--- conanfile.py
+++ conanfile.py
@@ -13,6 +13,13 @@ import subprocess
 import traceback
 import re
 
+def compatibility(os, compiler, compiler_version):
+    # On macos fallback to zlib apple-clang 13
+    if os == "Macos" and compiler == "apple-clang" and bool(re.match("14.*", compiler_version)):  
+        print("Compatibility match")
+        return ["zlib/1.3:compiler.version=13"]
+    return None
+    
 class HdpsCoreConan(ConanFile):
     """Class to package hdps-core using conan
 
