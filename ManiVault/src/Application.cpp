6dc82f3994c6084d6794cde4db687057b978ec83
Thomas Kroes <tkroes81@gmail.com>
2024-08-06 17:05:35 +0200

    Only load Font Awesome regular fonts on Windows
    The Mac OS and Linux font family names for regular and solid are the same: Font Awesome X Free. This makes it impossible to select the correct font family and assign the proper icon. At this point in time, we only use the solid icons, so we will only load the solid icons on Mac OS and Linux.

--- ManiVault/src/Application.cpp
+++ ManiVault/src/Application.cpp
@@ -49,15 +49,27 @@ Application::Application(int& argc, char** argv) :
     qDebug() << "Initializing icon fonts";
 
     _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(5, 14, { ":/IconFonts/FontAwesomeBrandsRegular-5.14.otf" }, false, "FontAwesomeBrands")));
-    //_iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(5, 14, { ":/IconFonts/FontAwesomeRegular-5.14.otf" }, false, "FontAwesomeRegular")));
+
+#if defined(_WIN32) || defined(_WIN64)
+    _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(5, 14, { ":/IconFonts/FontAwesomeRegular-5.14.otf" }, false, "FontAwesomeRegular")));
+#endif
+
     _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(5, 14, { ":/IconFonts/FontAwesomeSolid-5.14.otf" }, true, "FontAwesome")));
 
     _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 4, { ":/IconFonts/FontAwesomeBrandsRegular-6.4.otf" }, false, "FontAwesomeBrands")));
-    //_iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 4, { ":/IconFonts/FontAwesomeRegular-6.4.otf" }, false, "FontAwesomeRegular")));
+
+#if defined(_WIN32) || defined(_WIN64)
+    _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 4, { ":/IconFonts/FontAwesomeRegular-6.4.otf" }, false, "FontAwesomeRegular")));
+#endif
+
     _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 4, { ":/IconFonts/FontAwesomeSolid-6.4.otf" }, false, "FontAwesome")));
 
     _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 5, { ":/IconFonts/FontAwesomeBrandsRegular-6.5.otf" }, false, "FontAwesomeBrands")));
-    //_iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 5, { ":/IconFonts/FontAwesomeRegular-6.5.otf" }, false, "FontAwesomeRegular")));
+
+#if defined(_WIN32) || defined(_WIN64)
+    _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 5, { ":/IconFonts/FontAwesomeRegular-6.5.otf" }, false, "FontAwesomeRegular")));
+#endif
+
     _iconFonts.add(QSharedPointer<IconFont>(new FontAwesome(6, 5, { ":/IconFonts/FontAwesomeSolid-6.5.otf" }, false, "FontAwesome")));
 
     connect(Application::current(), &Application::coreManagersCreated, this, [this](CoreInterface* core) {
