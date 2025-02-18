5d303563367a456f30ddad586f633dbadae89091
Alexander Vieth <a.vieth@tudelft.nl>
2025-02-18 16:35:23 +0100

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -115,12 +115,12 @@ class HdpsCoreConan(ConanFile):
             # Ubuntu 24.04 ships with libicu 74 see https://launchpad.net/ubuntu/+source/icu
             # but either qt or cmake seems to require 73
             commands = [
-                "wget https://github.com/unicode-org/icu/releases/download/release-73-2/icu4c-73_2-Ubuntu22.04-x64.tgz",
-                "tar -xvf icu4c-73_2-Ubuntu22.04-x64.tgz",
-                "cp -r ./icu/usr/local/lib/* /usr/lib/x86_64-linux-gnu/",
-                "find ./icu/usr/local/lib/ -name \"*.73*\" -print0 | xargs -0 cp -r -t /usr/lib/x86_64-linux-gnu/",
-                "rm -rf ./icu/",
-                "rm -rf icu4c-73_2-Ubuntu22.04-x64.tgz"
+                "sudo -A wget -q https://github.com/unicode-org/icu/releases/download/release-73-2/icu4c-73_2-Ubuntu22.04-x64.tgz",
+                "sudo -A tar -xf icu4c-73_2-Ubuntu22.04-x64.tgz",
+                "sudo -A cp -r ./icu/usr/local/lib/* /usr/lib/x86_64-linux-gnu/",
+                "sudo -A find ./icu/usr/local/lib/ -name \"*.73*\" -print0 | xargs -0 cp -r -t /usr/lib/x86_64-linux-gnu/",
+                "sudo -A rm -rf ./icu/",
+                "sudo -A rm -rf icu4c-73_2-Ubuntu22.04-x64.tgz"
             ]
             for cmd in commands:
                 try:
