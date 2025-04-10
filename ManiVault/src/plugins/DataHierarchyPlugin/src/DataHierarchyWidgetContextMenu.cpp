f7b080c01c48b96c62004905d0e1293cd0b370bc
Thomas Kroes <tkroes81@gmail.com>
2025-04-10 15:46:33 +0200

    Some improvements
    - Use vertical and horizontal groups for dialog action layout
    - Adhere to Core code style
    - Update attribution

--- ManiVault/src/plugins/DataHierarchyPlugin/src/DataHierarchyWidgetContextMenu.cpp
+++ ManiVault/src/plugins/DataHierarchyPlugin/src/DataHierarchyWidgetContextMenu.cpp
@@ -467,63 +467,67 @@ QMenu* DataHierarchyWidgetContextMenu::getUnhideMenu()
 
 SelectionGroupIndexDialog::SelectionGroupIndexDialog(QWidget* parent) :
     QDialog(parent),
-    confirmButton(this, "Ok"),
-    selectionIndexAction(this, "Selection group index", -1, 250, -1)
+    _confirmButton(this, "Ok"),
+    _selectionIndexAction(this, "Set the same selection group index for all selected datasets", -1, 250, -1)
 {
     setWindowTitle(tr("Selection group index"));
     setWindowIcon(StyledIcon("ellipsis"));
     
-    QLabel* indicesLabel = new QLabel("Set the same selection group index for all selected datasets:");
+    _confirmButton.setEnabled(false);
+    _confirmButton.setToolTip("Selection group index must be larger than -1");
 
-    confirmButton.setEnabled(false);
-    confirmButton.setToolTip("Selection group index must be larger than -1");
-
-    connect(&confirmButton, &TriggerAction::triggered, this, &SelectionGroupIndexDialog::closeDialogAction);
+    connect(&_confirmButton, &TriggerAction::triggered, this, &SelectionGroupIndexDialog::closeDialogAction);
     connect(this, &SelectionGroupIndexDialog::closeDialog, this, &QDialog::accept);
 
-    connect(&selectionIndexAction, &IntegralAction::valueChanged, [this](int value) {
-        confirmButton.setEnabled(value >= 0);
+    connect(&_selectionIndexAction, &IntegralAction::valueChanged, [this](int value) {
+        _confirmButton.setEnabled(value >= 0);
         });
 
-    QHBoxLayout* layout = new QHBoxLayout();
-    layout->addWidget(indicesLabel);
-    layout->addWidget(selectionIndexAction.createWidget(this));
-    layout->addWidget(confirmButton.createWidget(this));
+    auto groupAction = new HorizontalGroupAction(this, "Settings");
+
+    groupAction->addAction(&_selectionIndexAction);
+    groupAction->addAction(&_confirmButton);
+
+    auto layout = new QHBoxLayout();
+
+    layout->addWidget(groupAction->createWidget(this));
+
     setLayout(layout);
 }
 
 SelectionPatternGroupIndexDialog::SelectionPatternGroupIndexDialog(QWidget* parent) :
     QDialog(parent),
-    confirmButton(this, "Ok"),
-    selectionPatternAction(this, "Pattern"),
-    selectionOptionAction(this, "Prefix/Suffix"),
-    selectionIndexAction(this, "Selection group starting index", -1, 1024, -1)
+    _confirmButton(this, "Ok"),
+    _selectionPatternAction(this, "Pattern"),
+    _selectionOptionAction(this, "Prefix/Suffix"),
+    _selectionIndexAction(this, "Start selection group indices at", -1, 1024, -1)
 {
     setWindowTitle(tr("Selection group pattern"));
     setWindowIcon(StyledIcon("ellipsis"));
     
-    QLabel* indicesLabel = new QLabel("Start selection group indices at:");
+    _confirmButton.setEnabled(false);
+    _confirmButton.setToolTip("Selection group index must be larger than -1");
 
-    confirmButton.setEnabled(false);
-    confirmButton.setToolTip("Selection group index must be larger than -1");
+    _selectionOptionAction.initialize({ "Suffix", "Prefix" }, "Suffix");
 
-    selectionOptionAction.initialize({ "Suffix", "Prefix" }, "Suffix");
-
-    connect(&confirmButton, &TriggerAction::triggered, this, &SelectionPatternGroupIndexDialog::closeDialogAction);
+    connect(&_confirmButton, &TriggerAction::triggered, this, &SelectionPatternGroupIndexDialog::closeDialogAction);
     connect(this, &SelectionPatternGroupIndexDialog::closeDialog, this, &QDialog::accept);
 
-    connect(&selectionIndexAction, &IntegralAction::valueChanged, [this](int value) {
-        confirmButton.setEnabled(value >= 0);
+    connect(&_selectionIndexAction, &IntegralAction::valueChanged, [this](int value) {
+        _confirmButton.setEnabled(value >= 0);
         });
 
-    int row = 0; // start a new row with ++row
-    QGridLayout* layout = new QGridLayout();
-    layout->addWidget(selectionPatternAction.createLabelWidget(this), row, 0);
-    layout->addWidget(selectionPatternAction.createWidget(this), row, 1);
-    layout->addWidget(selectionOptionAction.createLabelWidget(this), ++row, 0);
-    layout->addWidget(selectionOptionAction.createWidget(this), row, 1);
-    layout->addWidget(indicesLabel, ++row, 0);
-    layout->addWidget(selectionIndexAction.createWidget(this), row, 1);
-    layout->addWidget(confirmButton.createWidget(this), ++row, 1);
-    setLayout(layout);
+
+    auto settingsGroupAction = new VerticalGroupAction(this, "Settings");
+
+    settingsGroupAction->addAction(&_selectionPatternAction);
+    settingsGroupAction->addAction(&_selectionOptionAction);
+    settingsGroupAction->addAction(&_selectionIndexAction);
+    settingsGroupAction->addAction(&_confirmButton);
+
+    auto layout = new QVBoxLayout();
+
+    layout->addWidget(settingsGroupAction->createWidget(this));
+
+	setLayout(layout);
 }
