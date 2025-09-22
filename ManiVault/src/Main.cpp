8efbcb61d1879ca3a82d61a67fea638a6ae2dc81
Thomas Kroes <tkroes81@gmail.com>
2025-09-22 08:58:02 +0200

    Refactor splash screen action initialization
    Replaces direct SplashScreenAction instantiation with retrieval via application configuration chain. This improves consistency and centralizes splash screen action management.

--- ManiVault/src/Main.cpp
+++ ManiVault/src/Main.cpp
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 
     core.createManagers();
 
-    SplashScreenAction splashScreenAction(&application, true);
+    auto& splashScreenAction = application.getConfigurationAction().getBrandingConfigurationAction().getSplashScreenAction();
 
     splashScreenAction.getOpenAction().trigger();
 
