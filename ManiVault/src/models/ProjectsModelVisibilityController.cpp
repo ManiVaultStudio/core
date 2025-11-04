e04903601e53f7ceda1c92e3c1397e8efee6f7b6
Thomas Kroes <tkroes81@gmail.com>
2025-11-04 10:53:47 +0100

    Add ProjectsModelVisibilityController filter model
    Introduced ProjectsModelVisibilityController (ProjectsFilterModel) for advanced filtering and sorting of project database entries. Updated CMake configuration to include new source and header files.

---
+++ ManiVault/src/models/ProjectsModelVisibilityController.cpp
@@ -0,0 +1,188 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later 
+// A corresponding LICENSE file is located in the root directory of this source tree 
+// Copyright (C) 2023 BioVault (Biomedical Visual Analytics Unit LUMC - TU Delft) 
+
+#include "ProjectsFilterModel.h"
+#include "AbstractProjectsModel.h"
+
+#include "util/Version.h"
+
+#include "Application.h"
+
+#include <QDebug>
+
+#ifdef _DEBUG
+    #define PROJECTS_FILTER_MODEL_VERBOSE
+#endif
+
+using namespace mv::gui;
+using namespace mv::util;
+
+namespace mv {
+
+ProjectsFilterModel::ProjectsFilterModel(QObject* parent /*= nullptr*/) :
+    SortFilterProxyModel(parent, "ProjectsFilterModel"),
+    _projectDatabaseModel(nullptr),
+    _tagsFilterAction(this, "Tags filter"),
+    _excludeTagsFilterAction(this, "Exclude tags filter"),
+    _targetAppVersionAction(this, "App version"),
+    _filterLoadableOnlyAction(this, "Loadable only", true),
+    _filterStartupOnlyAction(this, "Startup only", false),
+    _filterGroupAction(this, "Filter group")
+{
+    setDynamicSortFilter(true);
+    setRecursiveFilteringEnabled(true);
+    setRowTypeName("Project");
+
+    _tagsFilterAction.setIconByName("tag");
+    _tagsFilterAction.setConfigurationFlag(WidgetAction::ConfigurationFlag::ForceCollapsedInGroup);
+    _tagsFilterAction.setPopupSizeHint(QSize(400, 0));
+
+    const auto applicationVersion = Application::current()->getVersion();
+
+    _targetAppVersionAction.setToolTip("Minimum ManiVault Studio version");
+    _targetAppVersionAction.getSuffixAction().setVisible(false);
+
+    _filterGroupAction.setIconByName("filter");
+    _filterGroupAction.setPopupSizeHint({ 400, 0 });
+
+    connect(&_tagsFilterAction, &OptionsAction::selectedOptionsChanged, this, &ProjectsFilterModel::invalidate);
+    connect(&_excludeTagsFilterAction, &OptionsAction::selectedOptionsChanged, this, &ProjectsFilterModel::invalidate);
+    connect(&_targetAppVersionAction, &VersionAction::versionChanged, this, &ProjectsFilterModel::invalidate);
+    connect(&_filterLoadableOnlyAction, &ToggleAction::toggled, this, &ProjectsFilterModel::invalidate);
+    connect(&_filterStartupOnlyAction, &ToggleAction::toggled, this, &ProjectsFilterModel::invalidate);
+
+    _filterGroupAction.setConfigurationFlag(WidgetAction::ConfigurationFlag::ForceCollapsedInGroup);
+
+    _filterGroupAction.addAction(&getFilterLoadableOnlyAction());
+    _filterGroupAction.addAction(&getTextFilterCaseSensitiveAction());
+    _filterGroupAction.addAction(&getTextFilterRegularExpressionAction());
+    _filterGroupAction.addAction(&getTargetAppVersionAction());
+}
+
+bool ProjectsFilterModel::filterAcceptsRow(int row, const QModelIndex& parent) const
+{
+    const auto index = sourceModel()->index(row, 0, parent);
+
+    if (!index.isValid())
+        return false;
+
+    if (!index.siblingAtColumn(static_cast<int>(AbstractProjectsModel::Column::IsVisible)).data(Qt::EditRole).toBool())
+        return false;
+
+    const auto isGroup = index.siblingAtColumn(static_cast<int>(AbstractProjectsModel::Column::IsGroup)).data(Qt::EditRole).toBool();
+
+    if (filterRegularExpression().isValid()) {
+        const auto key = index.siblingAtColumn(filterKeyColumn()).data(filterRole()).toString();
+
+        if (!key.contains(filterRegularExpression())) {
+            if (isGroup && hasAcceptedChildren(index))
+                return true;
+
+            return false;
+        }
+    }
+
+    if (!isGroup) {
+        const auto tagsList         = index.siblingAtColumn(static_cast<int>(AbstractProjectsModel::Column::Tags)).data(Qt::EditRole).toStringList();
+        const auto filterTagsList   = _tagsFilterAction.getSelectedOptions();
+
+        if (_tagsFilterAction.hasSelectedOptions()) {
+            bool matchTags = std::any_of(tagsList.begin(), tagsList.end(), [&](const QString& tag) {
+                return filterTagsList.contains(tag);
+            });
+
+            if (!matchTags)
+                return false;
+        }
+
+        const auto projectMinimumCoreVersion = index.siblingAtColumn(static_cast<int>(AbstractProjectsModel::Column::MinimumCoreVersion)).data(Qt::EditRole).value<Version>();
+
+        const Version targetAppVersion(_targetAppVersionAction.getMajor(), _targetAppVersionAction.getMinor(), 0);
+
+        if (targetAppVersion > projectMinimumCoreVersion)
+            return false;
+
+        const auto missingPlugins = index.siblingAtColumn(static_cast<int>(AbstractProjectsModel::Column::MissingPlugins)).data(Qt::EditRole).toStringList();
+
+        if (_filterLoadableOnlyAction.isChecked() && !missingPlugins.isEmpty())
+            return false;
+
+        const auto isStartup = index.siblingAtColumn(static_cast<int>(AbstractProjectsModel::Column::IsStartup)).data(Qt::EditRole).toBool();
+
+        if (_filterStartupOnlyAction.isChecked() && !isStartup)
+            return false;
+    } else {
+        return hasAcceptedChildren(index);
+    }
+
+    return true;
+}
+
+void ProjectsFilterModel::setSourceModel(QAbstractItemModel* sourceModel)
+{
+    SortFilterProxyModel::setSourceModel(sourceModel);
+
+    _projectDatabaseModel = dynamic_cast<AbstractProjectsModel*>(sourceModel);
+
+    const auto updateTags = [this]() -> void {
+        const auto uniqueTags = QStringList(_projectDatabaseModel->getTagsSet().begin(), _projectDatabaseModel->getTagsSet().end());
+
+        if (uniqueTags == _tagsFilterAction.getOptions())
+            return;
+
+        const auto firstTime = _tagsFilterAction.getOptions().isEmpty();
+
+        _tagsFilterAction.setOptions(uniqueTags);
+        _tagsFilterAction.setSelectedOptions(_tagsFilterAction.hasSelectedOptions() ? _tagsFilterAction.getSelectedOptions() : uniqueTags);
+    };
+
+    connect(_projectDatabaseModel, &AbstractProjectsModel::tagsChanged, this, updateTags);
+    connect(_projectDatabaseModel, &AbstractProjectsModel::populated, this, &SortFilterProxyModel::invalidate);
+
+    updateTags();
+}
+
+bool ProjectsFilterModel::lessThan(const QModelIndex& lhs, const QModelIndex& rhs) const
+{
+    return lhs.data().toString() < rhs.data().toString();
+}
+
+bool ProjectsFilterModel::hasAcceptedChildren(const QModelIndex& parent) const
+{
+    const auto model    = sourceModel();
+    const auto rowCount = model->rowCount(parent);
+
+    for (int i = 0; i < rowCount; ++i) {
+        const auto child = model->index(i, 0, parent);
+
+        if (filterAcceptsRow(i, parent) || hasAcceptedChildren(child))
+            return true;
+    }
+
+    return false;
+}
+
+void ProjectsFilterModel::fromVariantMap(const QVariantMap& variantMap)
+{
+    SortFilterProxyModel::fromVariantMap(variantMap);
+
+    _tagsFilterAction.fromParentVariantMap(variantMap);
+    _excludeTagsFilterAction.fromParentVariantMap(variantMap);
+    _targetAppVersionAction.fromParentVariantMap(variantMap);
+    _filterLoadableOnlyAction.fromParentVariantMap(variantMap);
+}
+
+QVariantMap ProjectsFilterModel::toVariantMap() const
+{
+    auto variantMap = SortFilterProxyModel::toVariantMap();
+
+    _tagsFilterAction.insertIntoVariantMap(variantMap);
+    _excludeTagsFilterAction.insertIntoVariantMap(variantMap);
+    _targetAppVersionAction.insertIntoVariantMap(variantMap);
+    _filterLoadableOnlyAction.insertIntoVariantMap(variantMap);
+
+    return variantMap;
+}
+
+}
---
+++ ManiVault/src/models/ProjectsModelVisibilityController.h
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later 
+// A corresponding LICENSE file is located in the root directory of this source tree 
+// Copyright (C) 2023 BioVault (Biomedical Visual Analytics Unit LUMC - TU Delft) 
+
+#pragma once
+
+#include "ManiVaultGlobals.h"
+
+#include "models/SortFilterProxyModel.h"
+
+#include "actions/OptionsAction.h"
+#include "actions/VersionAction.h"
+
+namespace mv {
+
+class AbstractProjectsModel;
+
+/**
+ * Project database filter model class
+ *
+ * Sorting and filtering model for the project database model
+ *
+ * @author Thomas Kroes
+ */
+class CORE_EXPORT ProjectsFilterModel : public SortFilterProxyModel
+{
+public:
+
+    /** 
+     * Construct with pointer to \p parent object
+     * @param parent Pointer to parent object
+    */
+    ProjectsFilterModel(QObject* parent = nullptr);
+
+    /**
+     * Returns whether \p row with \p parent is filtered out (false) or in (true)
+     * @param row Row index
+     * @param parent Parent index
+     * @return Boolean indicating whether the item is filtered in or out
+     */
+    bool filterAcceptsRow(int row, const QModelIndex& parent) const override;
+
+    /**
+     * Set source model to \p sourceModel
+     * @param sourceModel Pointer to source model
+     */
+    void setSourceModel(QAbstractItemModel* sourceModel) override;
+
+    /**
+     * Compares two model indices plugin \p lhs with \p rhs
+     * @param lhs Left-hand model index
+     * @param rhs Right-hand model index
+     */
+    bool lessThan(const QModelIndex& lhs, const QModelIndex& rhs) const override;
+
+private:
+
+    /**
+     * Get whether the parent has accepted children
+     * @param parent Parent model index
+     * @return Boolean indicating whether the parent has accepted children
+     */
+    bool hasAcceptedChildren(const QModelIndex& parent) const;
+
+public: // Serialization
+
+    /**
+     * Load from variant map
+     * @param variantMap Variant map
+     */
+    void fromVariantMap(const QVariantMap& variantMap) override;
+
+    /**
+     * Save to variant map
+     * @return Variant map
+     */
+    QVariantMap toVariantMap() const override;
+
+public: // Action getters
+
+    gui::OptionsAction& getTagsFilterAction() { return _tagsFilterAction; }
+    gui::OptionsAction& getExcludeTagsFilterAction() { return _excludeTagsFilterAction; }
+    gui::VersionAction& getTargetAppVersionAction() { return _targetAppVersionAction; }
+    gui::ToggleAction& getFilterLoadableOnlyAction() { return _filterLoadableOnlyAction; }
+    gui::ToggleAction& getFilterStartupOnlyAction() { return _filterStartupOnlyAction; }
+    gui::VerticalGroupAction& getFilterGroupAction() { return _filterGroupAction; }
+
+private:
+    AbstractProjectsModel*      _projectDatabaseModel;          /** Pointer to source model */
+    gui::OptionsAction          _tagsFilterAction;              /** Filter based on tag(s) */
+    gui::OptionsAction          _excludeTagsFilterAction;       /** Filter out based on tag(s) */
+    gui::VersionAction          _targetAppVersionAction;        /** Target app minimum version filter action */
+    gui::ToggleAction           _filterLoadableOnlyAction;      /** Filter out projects that are not loadable */
+    gui::ToggleAction           _filterStartupOnlyAction;       /** Filter out projects that are not startup projects */
+    gui::VerticalGroupAction    _filterGroupAction;             /** Groups the filter text, filter settings and minimum version settings */
+};
+
+}
