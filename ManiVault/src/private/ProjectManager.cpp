7e390960a61bb96fde853b05f9a9fff61d0f0492
Thomas Kroes <tkroes81@gmail.com>
2025-11-25 10:27:35 +0100

    Comment out splash screen action in publishProject
    The call to add the splash screen action to settingsGroupAction in publishProject was commented out to prevent it from being included during project publishing.

--- ManiVault/src/private/ProjectManager.cpp
+++ ManiVault/src/private/ProjectManager.cpp
@@ -1162,7 +1162,7 @@ void ProjectManager::publishProject(QString filePath /*= ""*/)
                     settingsGroupAction.addAction(&currentProject->getDescriptionAction());
                     settingsGroupAction.addAction(&currentProject->getTagsAction());
                     settingsGroupAction.addAction(&currentProject->getCommentsAction());
-                    settingsGroupAction.addAction(&currentProject->getSplashScreenAction());
+                    //settingsGroupAction.addAction(&currentProject->getSplashScreenAction());
                     //settingsGroupAction.addAction(&currentProject->getOverrideApplicationStatusBarAction());
                     //settingsGroupAction.addAction(&currentProject->getStatusBarVisibleAction());
                     settingsGroupAction.addAction(&currentProject->getAllowedPluginsOnlyAction());
