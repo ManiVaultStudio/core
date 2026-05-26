8776a097b7c5f02481f779e9c9e1b9be0c9a27f4
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-26 14:34:46 +0200

    Plugin info: image data

--- ManiVault/src/plugins/ImageData/src/ImageData.h
+++ ManiVault/src/plugins/ImageData/src/ImageData.h
@@ -125,7 +125,7 @@ class ImageDataFactory : public mv::plugin::RawDataFactory
     Q_INTERFACES(mv::plugin::RawDataFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "studio.manivault.ImageData"
-                      FILE  "ImageData.json")
+                      FILE  "PluginInfo.json")
 
 public:
     ImageDataFactory();
