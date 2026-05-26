79ee9f4cc37c2caa2736891f391fefdb0d161063
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-26 14:23:01 +0200

    fix updating plugininfo.json entry

--- ManiVault/src/plugins/DataHierarchyPlugin/src/DataHierarchyPlugin.h
+++ ManiVault/src/plugins/DataHierarchyPlugin/src/DataHierarchyPlugin.h
@@ -41,7 +41,7 @@ class DataHierarchyPluginFactory : public ViewPluginFactory
     Q_INTERFACES(mv::plugin::ViewPluginFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "studio.manivault.DataHierarchyPlugin"
-                      FILE  "DataHierarchyPlugin.json")
+                      FILE  "PluginInfo.json")
     
 public:
 
