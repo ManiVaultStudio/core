8433a17c4c72b0765cde2282aaa10b58dd971a2b
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-26 14:49:01 +0200

    Plugin info: tasks

--- ManiVault/src/plugins/TasksPlugin/src/TasksPlugin.h
+++ ManiVault/src/plugins/TasksPlugin/src/TasksPlugin.h
@@ -62,7 +62,7 @@ class TasksPluginFactory : public ViewPluginFactory
     Q_INTERFACES(mv::plugin::ViewPluginFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "studio.manivault.TasksPlugin"
-                      FILE  "TasksPlugin.json")
+                      FILE  "PluginInfo.json")
     
 public:
 
