c0035c69e41c9d27df60e0a27dd5b5d1cd91ef77
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-26 14:25:56 +0200

    Plugin info: cluster data

--- ManiVault/src/plugins/ClusterData/src/ClusterData.h
+++ ManiVault/src/plugins/ClusterData/src/ClusterData.h
@@ -286,7 +286,7 @@ class ClusterDataFactory : public mv::plugin::RawDataFactory
     Q_INTERFACES(mv::plugin::RawDataFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "studio.manivault.ClusterData"
-                      FILE  "ClusterData.json")
+                      FILE  "PluginInfo.json")
     
 public:
     ClusterDataFactory();
