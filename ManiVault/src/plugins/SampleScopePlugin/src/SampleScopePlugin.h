0f4fb98b870bfcdc52fcd1845451d5a849ea9a2b
Alexander Vieth <vieth.alexander@gmx.net>
2026-05-26 14:44:53 +0200

    plugin info: sample scope

--- ManiVault/src/plugins/SampleScopePlugin/src/SampleScopePlugin.h
+++ ManiVault/src/plugins/SampleScopePlugin/src/SampleScopePlugin.h
@@ -77,7 +77,7 @@ class SampleScopePluginFactory : public mv::plugin::ViewPluginFactory
     Q_INTERFACES(mv::plugin::ViewPluginFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "studio.manivault.SampleScopePlugin"
-                      FILE  "SampleScopePlugin.json")
+                      FILE  "PluginInfo.json")
     
 public:
 
